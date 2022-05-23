269018fd44d79963e5a2ab2cb57bcb4446168479
walee sebiiy <waleesebiiy@gmail.com>
2022-05-23 17:28:34 +0100

    adding async storage

---
+++ utils/asyncStorage.js
@@ -0,0 +1,48 @@
+import AsyncStorage from '@react-native-async-storage/async-storage'
+
+/**
+ * @description Sets the value for a key and invokes
+ * a callback upon completion.
+ * @param0 {string} key
+ */
+export const _storeValueByKey = async (key, value) => {
+  try {
+    await AsyncStorage.setItem(`@${key}`, JSON.stringify(value))
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+/**
+ * @description Fetches an item for a key and invokes
+ * a callback upon completion. Returns a Promise object.
+ * @param0 {string} key
+ */
+export const _retriveValueByKey = async (key) => {
+  try {
+    const value = await AsyncStorage.getItem(`@${key}`)
+    if (value !== null) {
+      return value
+    } else {
+      return null
+    }
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+/**
+ * @description Removes an item for a key and invokes
+ * a callback upon completion.
+ * @param0 {string} key
+ */
+export const _removeValueByKey = async (key) => {
+  try {
+    const value = await AsyncStorage.getItem(`@${key}`)
+    if (value !== null) {
+      await AsyncStorage.removeItem(`@${key}`)
+    }
+  } catch (error) {
+    console.log(error)
+  }
+}
