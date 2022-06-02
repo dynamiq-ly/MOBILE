d9877d27aa17a4e69832063e7026bb54dd608470
walee sebiiy <waleesebiiy@gmail.com>
2022-06-03 00:41:54 +0100

    adding icon to point of interest

--- components/point-interest/PointOfInterest.screen.js
+++ components/point-interest/PointOfInterest.screen.js
@@ -1,6 +1,10 @@
 import AreaView from 'utils/TabAreaView'
-import { GridLayout } from 'styles/grid.module'
+import Icon from 'react-native-remix-icon'
+import { fontPixel } from 'utils/normalization'
 import SquareCard from 'components/cards/SquareCard'
+
+import { CartView } from 'styles/cart.module'
+import { GridLayout } from 'styles/grid.module'
 import { Interesting_Point } from 'mock/interest_list'
 
 export default function PointInterestScreen() {
@@ -19,5 +23,17 @@ export default function PointInterestScreen() {
         })}
       </GridLayout>
     </AreaView>
+  )
+}
+
+/**
+ * @description icon for favorit in point of interest
+ * @returns {React.Component}
+ */
+export const PointInterestScreenOptions = ({ func }) => {
+  return (
+    <CartView onPress={func}>
+      <Icon name='ri-heart-line' size={fontPixel(24)} />
+    </CartView>
   )
 }
