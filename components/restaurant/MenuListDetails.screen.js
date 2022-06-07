b4b187030c4a32fecc3ff90e8106fe3e79df15d7
walee sebiiy <waleesebiiy@gmail.com>
2022-06-07 20:47:58 +0100

    update menu list detail screen

--- components/restaurant/MenuListDetails.screen.js
+++ components/restaurant/MenuListDetails.screen.js
@@ -1,7 +1,8 @@
-import AreaView from '~/utils/TabAreaView'
-import { Image } from 'styles/image.module'
 import Text from 'components/text/Text'
-import { StyledFullDetailedCardContent } from '~/styles/cards.module'
+import AreaView from '~/utils/TabAreaView'
+
+import { View } from 'react-native'
+import { Image } from 'styles/image.module'
 
 const MenuListDetailsScreen = ({ route }) => {
   const { _data } = route.params
@@ -12,21 +13,27 @@ const MenuListDetailsScreen = ({ route }) => {
           uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNTcNy1Prx33_8N0lgoeo-1XpFjW7bjwZPKA&usqp=CAU',
         }}
       />
-      <StyledFullDetailedCardContent>
-        <Text content={_data.title} size={20} weight={600} up={'cap'} />
+      <View
+        style={{
+          marginTop: 14,
+          flexDirection: 'row',
+          alignItems: 'center',
+          justifyContent: 'space-between',
+        }}>
+        <Text content={_data.title} size={24} weight={700} up={'cap'} />
         <Text
-          content={_data.price}
-          size={16}
+          content={`${_data.price}$`}
+          size={18}
           weight={600}
           up={'cap'}
           color={'dominant'}
         />
-      </StyledFullDetailedCardContent>
+      </View>
 
       <Text
         content={_data.description}
         size={16}
-        weight={600}
+        weight={400}
         up={'cap'}
         color={'gray'}
       />
