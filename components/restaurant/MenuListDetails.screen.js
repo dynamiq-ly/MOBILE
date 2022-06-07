d4dff1c8089a4417276521317f1c963451ed49fa
Manel Rejeb <91155171+Manel-Rejeb@users.noreply.github.com>
2022-06-07 21:42:28 +0200

    MenuListDetailscreen

--- components/restaurant/MenuListDetails.screen.js
+++ components/restaurant/MenuListDetails.screen.js
@@ -1,7 +1,37 @@
 import AreaView from '~/utils/TabAreaView'
+import { Image } from 'styles/image.module'
+import Text from 'components/text/Text'
+import { StyledFullDetailedCardContent } from '~/styles/cards.module'
 
-const MenuListDetailsScreen = () => {
-  return <AreaView></AreaView>
+const MenuListDetailsScreen = ({ route }) => {
+  const { _data } = route.params
+  return (
+    <AreaView>
+      <Image
+        source={{
+          uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNTcNy1Prx33_8N0lgoeo-1XpFjW7bjwZPKA&usqp=CAU',
+        }}
+      />
+      <StyledFullDetailedCardContent>
+        <Text content={_data.title} size={20} weight={600} up={'cap'} />
+        <Text
+          content={_data.price}
+          size={16}
+          weight={600}
+          up={'cap'}
+          color={'dominant'}
+        />
+      </StyledFullDetailedCardContent>
+
+      <Text
+        content={_data.description}
+        size={16}
+        weight={600}
+        up={'cap'}
+        color={'gray'}
+      />
+    </AreaView>
+  )
 }
 
 export default MenuListDetailsScreen
--- routes/MenuStack.js
+++ routes/MenuStack.js
@@ -78,7 +78,7 @@ const MenuStackNavigation = () => {
               component={el.component}
               options={{
                 header: el.header,
-                presentation: presentationModal(el.path),
+                // presentation: presentationModal(el.path),
               }}
             />
           )
