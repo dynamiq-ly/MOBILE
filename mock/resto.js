8a49632826040d42d1f95846ac143ce786ddea4a
walee sebiiy <waleesebiiy@gmail.com>
2022-06-14 00:19:09 +0100

    adding alcohol mocked data

--- mock/resto.js
+++ mock/resto.js
@@ -246,3 +246,326 @@ export const array_drinks_details_restaurant = [
       'Short, strong, dark coffee, made the traditional Italian way.',
   },
 ]
+
+export const array_drinks_alcohol_list = [
+  {
+    id: 1,
+    bottle_name: 'Liana',
+    bottle_date_made: 1996,
+    bottle_region: 'Norway',
+    bottle_description:
+      'Etiam vel augue. Vestibulum rutrum rutrum neque. Aenean auctor gravida sem. Praesent id massa id nisl venenatis lacinia. Aenean sit amet justo. Morbi ut odio. Cras mi pede, malesuada in, imperdiet et, commodo vulputate, justo. In blandit ultrices enim. Lorem ipsum dolor sit amet, consectetuer adipiscing elit.',
+    bottle_price_bottle: 112.86,
+    bottle_price_glass: 21.9,
+    bottle_glass_volume: 50.61,
+    bottle_barrel_old: 5,
+    bottle_alcohol_percentage: 88.89,
+    bottle_category: 'red wine',
+    bottle_image:
+      'https://www.pineridgevineyards.com/wp-content/uploads/2020/07/Napa-Cab_0937.jpg',
+  },
+  {
+    id: 2,
+    bottle_name: 'Cloe',
+    bottle_date_made: 1994,
+    bottle_region: 'Mongolia',
+    bottle_description:
+      'Integer ac neque. Duis bibendum. Morbi non quam nec dui luctus rutrum. Nulla tellus.',
+    bottle_price_bottle: 112.78,
+    bottle_price_glass: 17.27,
+    bottle_glass_volume: 71.02,
+    bottle_barrel_old: 1,
+    bottle_alcohol_percentage: 18.9,
+    bottle_category: 'red wine',
+    bottle_image:
+      'https://www.pineridgevineyards.com/wp-content/uploads/2020/07/Napa-Cab_0937.jpg',
+  },
+  {
+    id: 3,
+    bottle_name: 'Bridget',
+    bottle_date_made: 2004,
+    bottle_region: 'Indonesia',
+    bottle_description:
+      'Morbi sem mauris, laoreet ut, rhoncus aliquet, pulvinar sed, nisl. Nunc rhoncus dui vel sem. Sed sagittis. Nam congue, risus semper porta volutpat, quam pede lobortis ligula, sit amet eleifend pede libero quis orci. Nullam molestie nibh in lectus. Pellentesque at nulla. Suspendisse potenti. Cras in purus eu magna vulputate luctus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus vestibulum sagittis sapien.',
+    bottle_price_bottle: 389.06,
+    bottle_price_glass: 29.92,
+    bottle_glass_volume: 45.01,
+    bottle_barrel_old: 14,
+    bottle_alcohol_percentage: 59.65,
+    bottle_category: 'red wine',
+    bottle_image:
+      'https://www.pineridgevineyards.com/wp-content/uploads/2020/07/Napa-Cab_0937.jpg',
+  },
+  {
+    id: 4,
+    bottle_name: 'Lauree',
+    bottle_date_made: 2008,
+    bottle_region: 'South Africa',
+    bottle_description:
+      'Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede. Morbi porttitor lorem id ligula. Suspendisse ornare consequat lectus. In est risus, auctor sed, tristique in, tempus sit amet, sem. Fusce consequat. Nulla nisl. Nunc nisl. Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa.',
+    bottle_price_bottle: 224.72,
+    bottle_price_glass: 35.3,
+    bottle_glass_volume: 51.1,
+    bottle_barrel_old: 2,
+    bottle_alcohol_percentage: 38.2,
+    bottle_category: 'rose wine',
+    bottle_image:
+      'https://www.pineridgevineyards.com/wp-content/uploads/2020/07/Napa-Cab_0937.jpg',
+  },
+  {
+    id: 5,
+    bottle_name: 'Clayborn',
+    bottle_date_made: 2009,
+    bottle_region: 'Latvia',
+    bottle_description:
+      'Morbi non lectus. Aliquam sit amet diam in magna bibendum imperdiet. Nullam orci pede, venenatis non, sodales sed, tincidunt eu, felis.',
+    bottle_price_bottle: 292.48,
+    bottle_price_glass: 26.96,
+    bottle_glass_volume: 44.83,
+    bottle_barrel_old: 16,
+    bottle_alcohol_percentage: 51.13,
+    bottle_category: 'white wine',
+    bottle_image:
+      'https://www.pineridgevineyards.com/wp-content/uploads/2020/07/Napa-Cab_0937.jpg',
+  },
+  {
+    id: 6,
+    bottle_name: 'Yorke',
+    bottle_date_made: 2001,
+    bottle_region: 'Georgia',
+    bottle_description:
+      'Integer ac leo. Pellentesque ultrices mattis odio. Donec vitae nisi. Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla. Sed vel enim sit amet nunc viverra dapibus. Nulla suscipit ligula in lacus.',
+    bottle_price_bottle: 465.16,
+    bottle_price_glass: 10.88,
+    bottle_glass_volume: 41.17,
+    bottle_barrel_old: 17,
+    bottle_alcohol_percentage: 22.21,
+    bottle_category: 'cognac',
+    bottle_image:
+      'https://www.pineridgevineyards.com/wp-content/uploads/2020/07/Napa-Cab_0937.jpg',
+  },
+  {
+    id: 7,
+    bottle_name: 'Gale',
+    bottle_date_made: 1995,
+    bottle_region: 'Brazil',
+    bottle_description:
+      'Cras non velit nec nisi vulputate nonummy. Maecenas tincidunt lacus at velit. Vivamus vel nulla eget eros elementum pellentesque. Quisque porta volutpat erat. Quisque erat eros, viverra eget, congue eget, semper rutrum, nulla. Nunc purus.',
+    bottle_price_bottle: 376.27,
+    bottle_price_glass: 17.08,
+    bottle_glass_volume: 48.84,
+    bottle_barrel_old: 17,
+    bottle_alcohol_percentage: 81.48,
+    bottle_category: 'white wine',
+    bottle_image:
+      'https://www.pineridgevineyards.com/wp-content/uploads/2020/07/Napa-Cab_0937.jpg',
+  },
+  {
+    id: 8,
+    bottle_name: 'Eartha',
+    bottle_date_made: 2007,
+    bottle_region: 'Czech Republic',
+    bottle_description:
+      'Donec ut dolor. Morbi vel lectus in quam fringilla rhoncus. Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id, turpis. Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci. Mauris lacinia sapien quis libero.',
+    bottle_price_bottle: 208.81,
+    bottle_price_glass: 13.23,
+    bottle_glass_volume: 68.85,
+    bottle_barrel_old: 15,
+    bottle_alcohol_percentage: 62.46,
+    bottle_category: 'liqours',
+    bottle_image:
+      'https://www.pineridgevineyards.com/wp-content/uploads/2020/07/Napa-Cab_0937.jpg',
+  },
+  {
+    id: 9,
+    bottle_name: 'Jasun',
+    bottle_date_made: 1995,
+    bottle_region: 'Philippines',
+    bottle_description:
+      'Morbi sem mauris, laoreet ut, rhoncus aliquet, pulvinar sed, nisl. Nunc rhoncus dui vel sem. Sed sagittis. Nam congue, risus semper porta volutpat, quam pede lobortis ligula, sit amet eleifend pede libero quis orci. Nullam molestie nibh in lectus. Pellentesque at nulla. Suspendisse potenti.',
+    bottle_price_bottle: 277.38,
+    bottle_price_glass: 16.18,
+    bottle_glass_volume: 30.62,
+    bottle_barrel_old: 5,
+    bottle_alcohol_percentage: 85.89,
+    bottle_category: 'liqours',
+    bottle_image:
+      'https://www.pineridgevineyards.com/wp-content/uploads/2020/07/Napa-Cab_0937.jpg',
+  },
+  {
+    id: 10,
+    bottle_name: 'Arvin',
+    bottle_date_made: 1992,
+    bottle_region: 'Malta',
+    bottle_description:
+      'Ut tellus. Nulla ut erat id mauris vulputate elementum. Nullam varius.',
+    bottle_price_bottle: 261.66,
+    bottle_price_glass: 23.14,
+    bottle_glass_volume: 36.27,
+    bottle_barrel_old: 9,
+    bottle_alcohol_percentage: 71.73,
+    bottle_category: 'vodka',
+    bottle_image:
+      'https://www.pineridgevineyards.com/wp-content/uploads/2020/07/Napa-Cab_0937.jpg',
+  },
+  {
+    id: 11,
+    bottle_name: 'Johnathan',
+    bottle_date_made: 1991,
+    bottle_region: 'China',
+    bottle_description:
+      'Vestibulum ac est lacinia nisi venenatis tristique. Fusce congue, diam id ornare imperdiet, sapien urna pretium nisl, ut volutpat sapien arcu sed augue. Aliquam erat volutpat. In congue. Etiam justo.',
+    bottle_price_bottle: 418.41,
+    bottle_price_glass: 37.95,
+    bottle_glass_volume: 69.71,
+    bottle_barrel_old: 12,
+    bottle_alcohol_percentage: 43.32,
+    bottle_category: 'white wine',
+    bottle_image:
+      'https://www.pineridgevineyards.com/wp-content/uploads/2020/07/Napa-Cab_0937.jpg',
+  },
+  {
+    id: 12,
+    bottle_name: 'Rodney',
+    bottle_date_made: 1993,
+    bottle_region: 'Brazil',
+    bottle_description:
+      'Aliquam non mauris. Morbi non lectus. Aliquam sit amet diam in magna bibendum imperdiet.',
+    bottle_price_bottle: 336.48,
+    bottle_price_glass: 25.23,
+    bottle_glass_volume: 63.67,
+    bottle_barrel_old: 14,
+    bottle_alcohol_percentage: 24.91,
+    bottle_category: 'whisky',
+    bottle_image:
+      'https://www.pineridgevineyards.com/wp-content/uploads/2020/07/Napa-Cab_0937.jpg',
+  },
+  {
+    id: 13,
+    bottle_name: 'Hiram',
+    bottle_date_made: 1997,
+    bottle_region: 'Portugal',
+    bottle_description:
+      'Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo. Maecenas pulvinar lobortis est. Phasellus sit amet erat. Nulla tempus. Vivamus in felis eu sapien cursus vestibulum. Proin eu mi.',
+    bottle_price_bottle: 418.52,
+    bottle_price_glass: 22.66,
+    bottle_glass_volume: 33.39,
+    bottle_barrel_old: 19,
+    bottle_alcohol_percentage: 94.07,
+    bottle_category: 'whisky',
+    bottle_image:
+      'https://www.pineridgevineyards.com/wp-content/uploads/2020/07/Napa-Cab_0937.jpg',
+  },
+  {
+    id: 14,
+    bottle_name: 'Crystie',
+    bottle_date_made: 1995,
+    bottle_region: 'Vietnam',
+    bottle_description:
+      'Nunc nisl. Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa. Donec dapibus. Duis at velit eu est congue elementum.',
+    bottle_price_bottle: 453.38,
+    bottle_price_glass: 11.53,
+    bottle_glass_volume: 61.25,
+    bottle_barrel_old: 12,
+    bottle_alcohol_percentage: 92.0,
+    bottle_category: 'whisky',
+    bottle_image:
+      'https://www.pineridgevineyards.com/wp-content/uploads/2020/07/Napa-Cab_0937.jpg',
+  },
+  {
+    id: 15,
+    bottle_name: 'Gardiner',
+    bottle_date_made: 2007,
+    bottle_region: 'Japan',
+    bottle_description:
+      'Aenean fermentum. Donec ut mauris eget massa tempor convallis. Nulla neque libero, convallis eget, eleifend luctus, ultricies eu, nibh. Quisque id justo sit amet sapien dignissim vestibulum.',
+    bottle_price_bottle: 358.47,
+    bottle_price_glass: 30.05,
+    bottle_glass_volume: 69.31,
+    bottle_barrel_old: 1,
+    bottle_alcohol_percentage: 50.38,
+    bottle_category: 'rose wine',
+    bottle_image:
+      'https://www.pineridgevineyards.com/wp-content/uploads/2020/07/Napa-Cab_0937.jpg',
+  },
+  {
+    id: 16,
+    bottle_name: 'Chelsea',
+    bottle_date_made: 1994,
+    bottle_region: 'Vietnam',
+    bottle_description:
+      'In congue. Etiam justo. Etiam pretium iaculis justo. In hac habitasse platea dictumst. Etiam faucibus cursus urna. Ut tellus. Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi. Cras non velit nec nisi vulputate nonummy.',
+    bottle_price_bottle: 358.75,
+    bottle_price_glass: 13.09,
+    bottle_glass_volume: 49.05,
+    bottle_barrel_old: 17,
+    bottle_alcohol_percentage: 35.44,
+    bottle_category: 'whisky',
+    bottle_image:
+      'https://www.pineridgevineyards.com/wp-content/uploads/2020/07/Napa-Cab_0937.jpg',
+  },
+  {
+    id: 17,
+    bottle_name: 'Nedda',
+    bottle_date_made: 2011,
+    bottle_region: 'Djibouti',
+    bottle_description:
+      'Cras non velit nec nisi vulputate nonummy. Maecenas tincidunt lacus at velit. Vivamus vel nulla eget eros elementum pellentesque. Quisque porta volutpat erat. Quisque erat eros, viverra eget, congue eget, semper rutrum, nulla. Nunc purus. Phasellus in felis. Donec semper sapien a libero. Nam dui.',
+    bottle_price_bottle: 273.31,
+    bottle_price_glass: 33.7,
+    bottle_glass_volume: 53.4,
+    bottle_barrel_old: 14,
+    bottle_alcohol_percentage: 19.72,
+    bottle_category: 'liqours',
+    bottle_image:
+      'https://www.pineridgevineyards.com/wp-content/uploads/2020/07/Napa-Cab_0937.jpg',
+  },
+  {
+    id: 18,
+    bottle_name: 'Dianne',
+    bottle_date_made: 1995,
+    bottle_region: 'France',
+    bottle_description:
+      'Aliquam sit amet diam in magna bibendum imperdiet. Nullam orci pede, venenatis non, sodales sed, tincidunt eu, felis. Fusce posuere felis sed lacus. Morbi sem mauris, laoreet ut, rhoncus aliquet, pulvinar sed, nisl. Nunc rhoncus dui vel sem. Sed sagittis. Nam congue, risus semper porta volutpat, quam pede lobortis ligula, sit amet eleifend pede libero quis orci. Nullam molestie nibh in lectus.',
+    bottle_price_bottle: 422.17,
+    bottle_price_glass: 13.38,
+    bottle_glass_volume: 68.6,
+    bottle_barrel_old: 9,
+    bottle_alcohol_percentage: 88.95,
+    bottle_category: 'vodka',
+    bottle_image:
+      'https://www.pineridgevineyards.com/wp-content/uploads/2020/07/Napa-Cab_0937.jpg',
+  },
+  {
+    id: 19,
+    bottle_name: 'Harman',
+    bottle_date_made: 1995,
+    bottle_region: 'Iraq',
+    bottle_description:
+      'In hac habitasse platea dictumst. Maecenas ut massa quis augue luctus tincidunt. Nulla mollis molestie lorem. Quisque ut erat. Curabitur gravida nisi at nibh.',
+    bottle_price_bottle: 442.23,
+    bottle_price_glass: 12.63,
+    bottle_glass_volume: 61.63,
+    bottle_barrel_old: 7,
+    bottle_alcohol_percentage: 11.68,
+    bottle_category: 'red wine',
+    bottle_image:
+      'https://www.pineridgevineyards.com/wp-content/uploads/2020/07/Napa-Cab_0937.jpg',
+  },
+  {
+    id: 20,
+    bottle_name: 'Selestina',
+    bottle_date_made: 2003,
+    bottle_region: 'Kazakhstan',
+    bottle_description:
+      'Vivamus tortor. Duis mattis egestas metus. Aenean fermentum. Donec ut mauris eget massa tempor convallis. Nulla neque libero, convallis eget, eleifend luctus, ultricies eu, nibh. Quisque id justo sit amet sapien dignissim vestibulum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla dapibus dolor vel est.',
+    bottle_price_bottle: 118.13,
+    bottle_price_glass: 14.84,
+    bottle_glass_volume: 63.86,
+    bottle_barrel_old: 10,
+    bottle_alcohol_percentage: 44.24,
+    bottle_category: 'liquors',
+    bottle_image:
+      'https://www.pineridgevineyards.com/wp-content/uploads/2020/07/Napa-Cab_0937.jpg',
+  },
+]
