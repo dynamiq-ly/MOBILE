2c53a80c97e9ffea8217f87753b067e81d61011d
walee sebiiy <waleesebiiy@gmail.com>
2022-06-15 17:21:07 +0100

    remove global refresh controll

--- utils/TabAreaView.js
+++ utils/TabAreaView.js
@@ -1,7 +1,5 @@
-import { useCallback, useState } from 'react'
 import { StatusBar } from 'expo-status-bar'
 import { Container } from 'styles/app.module'
-import { RefreshControl } from 'react-native'
 
 /**
  * @param0 {FC} children
@@ -15,25 +13,10 @@ export default function AreaView({
   statusBar = false,
   ...rest
 }) {
-  const [refreshing, setRefreshing] = useState(false)
-
-  const onRefresh = useCallback(() => {
-    setRefreshing(true)
-    wait().then(() => setRefreshing(false))
-  }, [])
-
   return (
-    <Container
-      {...rest}
-      refreshControl={
-        <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
-      }>
+    <Container {...rest}>
       <StatusBar style={mode} hidden={statusBar} />
       {children}
     </Container>
   )
-}
-
-const wait = () => {
-  return new Promise((resolve) => setTimeout(resolve, 1000))
 }
