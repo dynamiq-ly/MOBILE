85c325696295b48a37323920710a4fcb99eac4a2
walee sebiiy <waleesebiiy@gmail.com>
2022-06-15 22:58:42 +0100

    adding refresh control

--- components/swimming-pool/SwimmingPoolLists.screen.js
+++ components/swimming-pool/SwimmingPoolLists.screen.js
@@ -3,25 +3,29 @@ import WeatherCard from 'components/cards/WeatherCard'
 import FullDetailedCard from 'components/cards/FullDetailedCard'
 
 import { useQuery } from 'react-query'
+import { RefreshControl } from 'react-native'
+import { useCallback, useState } from 'react'
 
 export default function SwimmingPoolListsScreen({ route }) {
   const { _data } = route.params
+  const { data, refetch } = useQuery('@weather', fetchWeather)
+  const [refresh, setRefresh] = useState(false)
 
-  const { data } = useQuery('@weather', () => {
-    return fetch(
-      'http://api.weatherapi.com/v1/current.json?key=bfb63b0183bf4384969123512201010&q=36.8392,10.1577&aqi=no'
-    ).then((res) => {
-      return res.json()
-    })
-  })
+  let onRefresh = useCallback(() => {
+    setRefresh(true)
+    refetch().then(() => setRefresh(false))
+  }, [])
 
   return (
-    <AreaView>
+    <AreaView
+      refreshControl={
+        <RefreshControl refreshing={refresh} onRefresh={onRefresh} />
+      }>
       <WeatherCard
-        temp_c={data.current.temp_c}
-        feels_like={data.current.feelslike_c}
-        weather_icon={data.current.condition.icon}
-        weather_descriptions={data.current.condition.text}
+        temp_c={data?.current.temp_c}
+        feels_like={data?.current.feelslike_c}
+        weather_icon={data?.current.condition.icon}
+        weather_descriptions={data?.current.condition.text}
       />
       {_data.map((item, index) => {
         return (
@@ -35,4 +39,12 @@ export default function SwimmingPoolListsScreen({ route }) {
       })}
     </AreaView>
   )
+}
+
+let fetchWeather = function () {
+  return fetch(
+    'http://api.weatherapi.com/v1/current.json?key=bfb63b0183bf4384969123512201010&q=36.8392,10.1577&aqi=no'
+  ).then((res) => {
+    return res.json()
+  })
 }
