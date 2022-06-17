5e87908b1518fd24e05899b22933e1b0029aca39
walee sebiiy <waleesebiiy@gmail.com>
2022-06-17 17:41:14 +0100

    remove brackets

--- components/checkbox/Radio.js
+++ components/checkbox/Radio.js
@@ -1,13 +1,5 @@
 import { Pressable } from 'react-native'
 
 export default function Radio({ checked = false, onChange, children }) {
-  return (
-    <Pressable
-      onPress={() => {
-        onChange(!checked)
-      }}
-    >
-      {children}
-    </Pressable>
-  )
+  return <Pressable onPress={() => onChange(!checked)}>{children}</Pressable>
 }
