1f095e4e6d7a53d9b658ed8abcc5fb8a61e696c6
walee sebiiy <waleesebiiy@gmail.com>
2022-06-17 21:22:10 +0100

    adding corner border to alcohol drinks

--- components/restaurant/AcloholDrinkDetails.js
+++ components/restaurant/AcloholDrinkDetails.js
@@ -6,7 +6,7 @@ import CloseHeader from 'components/header/CloseHeader'
 import RadioButtonCard from 'components/cards/RadioButtonCard'
 
 import { useState } from 'react'
-import { View } from '~/styles/detail.module'
+import { RadiusView, View } from '~/styles/detail.module'
 import { View as Gap } from 'react-native'
 import {
   Image,
@@ -21,45 +21,47 @@ const AcloholDrinkDetails = ({ route }) => {
     <View>
       <CloseHeader />
       <Image source={{ uri: _data.bottle_image }} />
-      <AreaView>
-        <Text content={_data.bottle_name} weight={700} up={'cap'} size={32} />
-        <Text
-          content={`${_data.bottle_region} ${_data.bottle_date_made}`}
-          up={'cap'}
-          size={21}
-        />
+      <RadiusView>
+        <AreaView>
+          <Text content={_data.bottle_name} weight={700} up={'cap'} size={32} />
+          <Text
+            content={`${_data.bottle_region} ${_data.bottle_date_made}`}
+            up={'cap'}
+            size={21}
+          />
 
-        <Gap style={{ marginBottom: 10 }} />
-        <Text content={_data.bottle_description} color={'gray'} size={16} />
-        <SafeAreaRowWrapperDetail>
-          <Radio
-            children={
-              <RadioButtonCard
-                name={'Bottle'}
-                price={_data.bottle_price_bottle}
-                image={
-                  'https://media.istockphoto.com/photos/red-wine-bottle-picture-id987571978?k=20&m=987571978&s=612x612&w=0&h=zGIYQaDvaDeuolW_AHecpQEhzEsPSDYC-7fBiJSak10='
-                }
-                state={type === 'bottle' && true}
-              />
-            }
-            onPress={() => setType('bottle')}
-          />
-          <Radio
-            children={
-              <RadioButtonCard
-                name={'Glass'}
-                price={_data.bottle_price_glass}
-                image={
-                  'https://media.istockphoto.com/photos/glass-of-red-wine-on-white-picture-id515707206?k=20&m=515707206&s=612x612&w=0&h=tii_cH6HuhU2SPSqsCDbX___6XMwRQmOd3d4hSRitPI='
-                }
-                state={type === 'glass' && true}
-              />
-            }
-            onPress={() => setType('glass')}
-          />
-        </SafeAreaRowWrapperDetail>
-      </AreaView>
+          <Gap style={{ marginBottom: 10 }} />
+          <Text content={_data.bottle_description} color={'gray'} size={16} />
+          <SafeAreaRowWrapperDetail>
+            <Radio
+              children={
+                <RadioButtonCard
+                  name={'Bottle'}
+                  price={_data.bottle_price_bottle}
+                  image={
+                    'https://media.istockphoto.com/photos/red-wine-bottle-picture-id987571978?k=20&m=987571978&s=612x612&w=0&h=zGIYQaDvaDeuolW_AHecpQEhzEsPSDYC-7fBiJSak10='
+                  }
+                  state={type === 'bottle' && true}
+                />
+              }
+              onPress={() => setType('bottle')}
+            />
+            <Radio
+              children={
+                <RadioButtonCard
+                  name={'Glass'}
+                  price={_data.bottle_price_glass}
+                  image={
+                    'https://media.istockphoto.com/photos/glass-of-red-wine-on-white-picture-id515707206?k=20&m=515707206&s=612x612&w=0&h=tii_cH6HuhU2SPSqsCDbX___6XMwRQmOd3d4hSRitPI='
+                  }
+                  state={type === 'glass' && true}
+                />
+              }
+              onPress={() => setType('glass')}
+            />
+          </SafeAreaRowWrapperDetail>
+        </AreaView>
+      </RadiusView>
       <ButtonWrapperDetail>
         <Button title={`order ${type}`} />
       </ButtonWrapperDetail>
