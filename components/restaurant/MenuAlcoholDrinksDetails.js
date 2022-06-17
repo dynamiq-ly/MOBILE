4c70d248dba41a97d3b5eda15ecbc88962bd1a80
walee sebiiy <waleesebiiy@gmail.com>
2022-06-17 14:00:49 +0100

    update category alcohol View height

--- components/restaurant/MenuAlcoholDrinksDetails.js
+++ components/restaurant/MenuAlcoholDrinksDetails.js
@@ -14,14 +14,13 @@ const MenuAlcoholDrinksDetails = ({ navigation }) => {
   const [isCategory, setCategory] = useState('red wine')
   return (
     <View>
-      <Gap>
+      <Gap style={{ height: 48, backgroundColor: 'red' }}>
         <HScrollView horizontal showsHorizontalScrollIndicator={false}>
           {Wine_category_Array.map((el, key) => {
             return (
               <Gap
                 style={{ alignItems: 'center', flexDirection: 'row' }}
-                key={key}
-              >
+                key={key}>
                 <FixedWidthButton
                   title={el}
                   func={() => setCategory(el)}
