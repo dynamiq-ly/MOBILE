e6e663898b226f0031d46ad65a3a7137870d62f9
Manel Rejeb <91155171+Manel-Rejeb@users.noreply.github.com>
2022-06-17 18:39:08 +0200

    adding radio button component

---
+++ components/checkbox/Radio.js
@@ -0,0 +1,13 @@
+import { Pressable } from 'react-native'
+
+export default function Radio({ checked = false, onChange, children }) {
+  return (
+    <Pressable
+      onPress={() => {
+        onChange(!checked)
+      }}
+    >
+      {children}
+    </Pressable>
+  )
+}
