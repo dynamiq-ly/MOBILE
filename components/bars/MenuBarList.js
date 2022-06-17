944c0958907784abc22153deb9b86073d490a58f
Manel Rejeb <91155171+Manel-Rejeb@users.noreply.github.com>
2022-06-17 21:37:40 +0200

    adding the MenuBarList Screen

--- components/bars/MenuBarList.js
+++ components/bars/MenuBarList.js
@@ -1,7 +1,16 @@
 import AreaView from 'utils/TabAreaView'
+import { array_menu_bar } from 'mock/resto'
+import SimpleCard from 'components/cards/SimpleCard'
+import { GridLayout } from 'styles/grid.module'
 
-const MenuBarList = () => {
-  return <AreaView></AreaView>
+export default function MenuBarScreen({ navigation }) {
+  return (
+    <AreaView>
+      <GridLayout>
+        {array_menu_bar.map((el, key) => {
+          return <SimpleCard key={key} title={el.name} image={el.image} />
+        })}
+      </GridLayout>
+    </AreaView>
+  )
 }
-
-export default MenuBarList
