1f8c2471b7cb034e631ea738ae8a54f7310bce3c
walee sebiiy <waleesebiiy@gmail.com>
2022-07-08 17:03:25 +0100

    adding room detail screen

---
+++ components/rooms/RoomDetail.screen.js
@@ -0,0 +1,228 @@
+import Text from 'components/text/Text'
+import AreaView from 'utils/TabAreaView'
+import Icon from 'react-native-remix-icon'
+import Carsouel from 'components/slider/carousel'
+import CloseHeader from 'components/header/CloseHeader'
+
+import { useState } from 'react'
+import { rooms } from 'mock/rooms'
+import { palette } from 'themes/palette'
+import { View as Gap } from 'react-native'
+import { StyledTagCards } from 'styles/cards.module'
+import {
+  HFLine,
+  RadiusView,
+  SafeAreaRowWrapperDetail,
+  View,
+} from 'styles/detail.module'
+
+export default function RoomDetailScreen() {
+  const [isRoom] = useState(rooms[0])
+  return (
+    <View>
+      <CloseHeader />
+      <Carsouel imageArray={isRoom.room_images} />
+      <RadiusView>
+        <AreaView mode={'light'}>
+          <SafeAreaRowWrapperDetail style={{ alignItems: 'flex-start' }}>
+            <Gap style={{ flex: 1 }}>
+              <Text
+                content={isRoom.room_name}
+                weight={600}
+                up={'cap'}
+                size={21}
+              />
+            </Gap>
+            <Gap
+              style={{
+                flexDirection: 'row',
+                alignItems: 'flex-end',
+              }}>
+              {isRoom.room_price_promotion > 1 ? (
+                <Text
+                  size={21}
+                  up={'cap'}
+                  weight={600}
+                  color={'dominant'}
+                  content={`${
+                    isRoom.room_price - (isRoom.room_price * 15) / 100
+                  }$`}
+                />
+              ) : (
+                <Text
+                  size={21}
+                  up={'cap'}
+                  weight={600}
+                  color={'dominant'}
+                  content={`${isRoom.room_price}$`}
+                />
+              )}
+
+              <Gap>
+                <Text size={14} up={'cap'} weight={500} content={'/night'} />
+              </Gap>
+            </Gap>
+          </SafeAreaRowWrapperDetail>
+          <Gap style={{ marginTop: 10 }} />
+          <Text size={16} color={'gray'} content={isRoom.room_description} />
+          <Gap style={{ marginTop: 10 }} />
+          <Gap style={{ marginTop: 10 }} />
+          <>
+            <SafeAreaRowWrapperDetail style={{ alignItems: 'center' }}>
+              <StyledTagCards>
+                <Icon
+                  name={'ri-map-pin-range-line'}
+                  color={palette.secondary.accent_500}
+                />
+                <Text
+                  size={14}
+                  up={'up'}
+                  weight={500}
+                  content={`${isRoom.room_location_floor} - ${isRoom.room_location_number}`}
+                />
+              </StyledTagCards>
+              <StyledTagCards>
+                <Icon
+                  name={
+                    isRoom.room_quality_smoking
+                      ? 'ri-shield-check-line'
+                      : 'ri-alarm-warning-line'
+                  }
+                  color={
+                    isRoom.room_quality_smoking
+                      ? palette.success.accent_500
+                      : palette.error.accent_500
+                  }
+                />
+                <Text size={14} up={'up'} weight={500} content={'smoking'} />
+              </StyledTagCards>
+
+              <StyledTagCards>
+                <Icon
+                  name={'ri-hotel-bed-line'}
+                  color={palette.secondary.accent_500}
+                />
+                <Text
+                  size={14}
+                  up={'up'}
+                  weight={500}
+                  content={isRoom.room_quality_size}
+                />
+              </StyledTagCards>
+              <StyledTagCards>
+                <Text
+                  size={21}
+                  up={'up'}
+                  weight={500}
+                  content={isRoom.room_quality_area}
+                />
+                <Text size={14} up={'up'} weight={500} content={'Area'} />
+              </StyledTagCards>
+            </SafeAreaRowWrapperDetail>
+          </>
+          <>
+            <HFLine />
+            <Text content={'occupency'} weight={500} size={18} up={'cap'} />
+            {isRoom.room_occupency.max_occupency > 0 && (
+              <SafeAreaRowWrapperDetail style={{ alignItems: 'center' }}>
+                <Text
+                  color={'gray'}
+                  content={'max occupency'}
+                  size={16}
+                  up={'cap'}
+                />
+                <Text
+                  content={isRoom.room_occupency.max_occupency}
+                  color={'dominant'}
+                  weight={500}
+                  size={16}
+                  up={'cap'}
+                />
+              </SafeAreaRowWrapperDetail>
+            )}
+
+            {isRoom.room_occupency.max_adult > 0 && (
+              <SafeAreaRowWrapperDetail style={{ alignItems: 'center' }}>
+                <Text
+                  color={'gray'}
+                  content={'max adult'}
+                  size={16}
+                  up={'cap'}
+                />
+                <Text
+                  content={isRoom.room_occupency.max_adult}
+                  color={'dominant'}
+                  weight={500}
+                  size={16}
+                  up={'cap'}
+                />
+              </SafeAreaRowWrapperDetail>
+            )}
+
+            {isRoom.room_occupency.max_children > 0 && (
+              <SafeAreaRowWrapperDetail style={{ alignItems: 'center' }}>
+                <Text
+                  color={'gray'}
+                  content={'max children'}
+                  size={16}
+                  up={'cap'}
+                />
+                <Text
+                  content={isRoom.room_occupency.max_children}
+                  color={'dominant'}
+                  weight={500}
+                  size={16}
+                  up={'cap'}
+                />
+              </SafeAreaRowWrapperDetail>
+            )}
+            {isRoom.room_occupency.max_guests > 0 && (
+              <SafeAreaRowWrapperDetail style={{ alignItems: 'center' }}>
+                <Text
+                  color={'gray'}
+                  content={'max guests'}
+                  size={16}
+                  up={'cap'}
+                />
+                <Text
+                  content={isRoom.room_occupency.max_guests}
+                  color={'dominant'}
+                  weight={500}
+                  size={16}
+                  up={'cap'}
+                />
+              </SafeAreaRowWrapperDetail>
+            )}
+          </>
+          {isRoom.room_features && (
+            <>
+              <HFLine />
+              <Text content={'features'} weight={500} size={18} up={'cap'} />
+              <Gap
+                style={{
+                  flexDirection: 'row',
+                  alignItems: 'center',
+                  marginTop: 10,
+                }}>
+                {isRoom.room_features.split(',').map((el, key) => (
+                  <Gap
+                    key={key}
+                    style={{
+                      backgroundColor: '#fffffe',
+                      marginRight: 5,
+                      borderRadius: 5,
+                      paddingVertical: 2,
+                      paddingHorizontal: 10,
+                    }}>
+                    <Text size={16} content={el} />
+                  </Gap>
+                ))}
+              </Gap>
+            </>
+          )}
+          <Gap style={{ marginBottom: 42 }} />
+        </AreaView>
+      </RadiusView>
+    </View>
+  )
+}
