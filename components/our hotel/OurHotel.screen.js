22b83d3bc37433995423330078e548da2b652d67
walee sebiiy <waleesebiiy@gmail.com>
2022-07-19 01:19:03 +0100

    adding hotel coutnry list

--- components/our hotel/OurHotel.screen.js
+++ components/our hotel/OurHotel.screen.js
@@ -1,5 +1,81 @@
+import Text from 'components/text/Text'
 import AreaView from 'utils/TabAreaView'
+import FullImageCard from 'components/cards/FullImageCard'
+import FixedWidthButton from 'components/button/FixedWidthButton'
+
+import { useState } from 'react'
+import { our_hotels } from 'mock/hotels'
+import { GridLayout } from 'styles/grid.module'
+import { View as Gap, ScrollView } from 'react-native'
+import { VerticalListLine } from 'styles/list.module'
 
 export default function OurHotelScreen() {
-  return <AreaView></AreaView>
+  const [hotels, setHotels] = useState({
+    category: 'all',
+    data: our_hotels,
+  })
+
+  return (
+    <AreaView>
+      <Text content={'Discover'} size={'21'} weight={600} up={'cap'} />
+      <Text
+        color={'gray'}
+        content={
+          'Our group offers a wide variety of hotels across the world, find your ideal location.'
+        }
+      />
+
+      <ScrollView
+        style={{ marginVertical: 14 }}
+        horizontal
+        showsHorizontalScrollIndicator={false}>
+        <Gap style={{ alignItems: 'center', flexDirection: 'row' }}>
+          <FixedWidthButton
+            title={'all'}
+            func={() => setHotels({ category: 'all', data: our_hotels })}
+            active={hotels.category !== 'all' ? true : false}
+          />
+          <VerticalListLine />
+        </Gap>
+        {our_hotels.map((el, key) => {
+          return (
+            <Gap
+              style={{ alignItems: 'center', flexDirection: 'row' }}
+              key={key}>
+              <FixedWidthButton
+                title={el.continent}
+                func={() =>
+                  setHotels({
+                    category: el.continent,
+                    data: our_hotels.filter(
+                      (elem) => elem.continent === el.continent
+                    ),
+                  })
+                }
+                active={hotels.category !== el.continent ? true : false}
+              />
+              {our_hotels.length !== key + 1 && <VerticalListLine />}
+            </Gap>
+          )
+        })}
+        <Gap style={{ marginRight: 5 }} />
+      </ScrollView>
+
+      <GridLayout>
+        {hotels.data.map((el) =>
+          el.countries.map((el, key) => {
+            return (
+              <FullImageCard
+                key={key}
+                title={el.country_name}
+                image={el.coutnry_thumbnail}
+                position={key % 2 === 1 && 'end'}
+                onPress={() => console.log('press')}
+              />
+            )
+          })
+        )}
+      </GridLayout>
+    </AreaView>
+  )
 }
---
+++ mock/hotels.js
@@ -0,0 +1,185 @@
+/**
+ * continent {
+ *  name: 'Africa',
+ * country : {
+ * name: 'Tunisia',
+ * city: {
+ * name: 'Tunis',}
+ * }
+ * }
+ */
+
+export const our_hotels = [
+  {
+    continent: 'Africa',
+    countries: [
+      {
+        country_name: 'Tunisia',
+        coutnry_thumbnail:
+          'https://specials-images.forbesimg.com/imageserve/5d540b9868cb0a000916fe6b/Four-Seasons-Hotel-Tunis/960x0.jpg?fit=scale',
+        cities: cities,
+      },
+      {
+        country_name: 'Egypt',
+        coutnry_thumbnail:
+          'https://media-cdn.tripadvisor.com/media/photo-s/1c/d9/a6/07/iberostar-selection-kuriat.jpg',
+        cities: cities,
+      },
+      {
+        country_name: 'Morocco',
+        coutnry_thumbnail:
+          'https://www.travelplusstyle.com/wp-content/uploads/2021/03/pink-hotels-amanjena-marrakech-1024x658.jpg',
+        cities: cities,
+      },
+      {
+        country_name: 'nigeria',
+        coutnry_thumbnail:
+          'https://media-cdn.tripadvisor.com/media/photo-s/16/f0/4a/57/hotel-exterior.jpg',
+        cities: cities,
+      },
+    ],
+  },
+  {
+    continent: 'Asia',
+    countries: [
+      {
+        country_name: 'Japan',
+        coutnry_thumbnail: 'https://www.japan-guide.com/g18/2025_top.jpg',
+        cities: cities,
+      },
+      {
+        country_name: 'India',
+        coutnry_thumbnail:
+          'https://tophotel.news/wp-content/uploads/2019/06/1539694286858-Taj-Mumbai-If-These-Walls-Could-Talk-7-of-12-1.jpg',
+        cities: cities,
+      },
+      {
+        country_name: 'Philipines',
+        coutnry_thumbnail:
+          'https://d27k8xmh3cuzik.cloudfront.net/wp-content/uploads/2018/04/SHANGRILA.jpg',
+        cities: cities,
+      },
+    ],
+  },
+  {
+    continent: 'Europe',
+    countries: [
+      {
+        country_name: 'France',
+        coutnry_thumbnail:
+          'https://viatravelers.com/wp-content/uploads/2020/06/shangrila_res-3.jpg.webp',
+        cities: cities,
+      },
+      {
+        country_name: 'Germany',
+        coutnry_thumbnail:
+          'https://thetripguider.com/wp-content/uploads/2021/04/Romantik_Hotel_Markusturm_Rothenburg.jpg',
+        cities: cities,
+      },
+      {
+        country_name: 'Italy',
+        coutnry_thumbnail:
+          'https://media-cdn.tripadvisor.com/media/photo-s/1b/5a/2e/9e/exterior.jpg',
+        cities: cities,
+      },
+      {
+        country_name: 'Spain',
+        coutnry_thumbnail:
+          'https://i0.wp.com/theluxurytravelexpert.com/wp-content/uploads/2022/03/spain-6.jpeg?fit=970%2C546&ssl=1',
+        cities: cities,
+      },
+      {
+        country_name: 'Finland',
+        coutnry_thumbnail:
+          'https://media-cdn.tripadvisor.com/media/photo-s/21/56/95/0b/kammi-family-suites-by.jpg',
+        cities: cities,
+      },
+    ],
+  },
+]
+
+const cities = [
+  {
+    city_name: 'Tunis',
+    regions: [
+      {
+        region_name: 'borj cedria',
+        hotels: hotels,
+      },
+      {
+        region_name: 'sidi bousid',
+        hotels: hotels,
+      },
+      {
+        region_name: 'carthage',
+        hotels: hotels,
+      },
+    ],
+  },
+  {
+    city_name: 'nabel',
+    regions: [
+      {
+        region_name: 'hammamet',
+        hotels: hotels,
+      },
+      {
+        region_name: 'klibia',
+        hotels: hotels,
+      },
+    ],
+  },
+
+  {
+    city_name: 'jerba',
+    regions: [
+      {
+        region_name: 'mazrane',
+        hotels: hotels,
+      },
+      {
+        region_name: 'aghir',
+        hotels: hotels,
+      },
+    ],
+  },
+]
+
+const hotels = [
+  {
+    hotel_name: 'hotel 1',
+    hotel_rating: '4.5',
+    hotel_price: '$100',
+    hotel_location: 'Tunis, Tunisia',
+    hotel_features: 'animation, wifi, pool, clubs, sightseeing',
+    hotel_discription:
+      'Ideally located near the charming historic village of Tabarka, on the splendid northwest coast of Tunisia, La Cigale Tabarka Hotel – Thalasso & Spa – Golf is a magical place where refined luxury, traditional hospitality and green nature combine...',
+  },
+  {
+    hotel_name: 'hotel 2',
+    hotel_rating: '2',
+    hotel_price: '$140',
+    hotel_location: 'carthage iin zaghouan',
+    hotel_features: 'animation, wifi, pool, clubs, sightseeing',
+    hotel_discription:
+      'Ideally located near the charming historic village of Tabarka, on the splendid northwest coast of Tunisia, La Cigale Tabarka Hotel – Thalasso & Spa – Golf is a magical place where refined luxury, traditional hospitality and green nature combine...',
+  },
+  {
+    hotel_name: 'hotel 3',
+    hotel_rating: '2',
+    hotel_price: '$440',
+    hotel_location: 'sidi boussid, sidi dhrif',
+    hotel_features: 'animation, wifi, pool, clubs, sightseeing',
+    hotel_discription:
+      'Ideally located near the charming historic village of Tabarka, on the splendid northwest coast of Tunisia, La Cigale Tabarka Hotel – Thalasso & Spa – Golf is a magical place where refined luxury, traditional hospitality and green nature combine...',
+  },
+  {
+    hotel_name: 'hotel 3',
+    hotel_rating: '2',
+    hotel_price: '$320',
+    hotel_location: 'yassmine hammamet nabel',
+    hotel_features: 'animation, wifi, pool, clubs, sightseeing',
+    hotel_discription:
+      'Ideally located near the charming historic village of Tabarka, on the splendid northwest coast of Tunisia, La Cigale Tabarka Hotel – Thalasso & Spa – Golf is a magical place where refined luxury, traditional hospitality and green nature combine...',
+  },
+]
