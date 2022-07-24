c7683b8c3e23a64239e159ab002a22c9b310b79f
walee sebiiy <waleesebiiy@gmail.com>
2022-07-24 14:44:50 +0100

    update header size

--- components/header/TextHeader.js
+++ components/header/TextHeader.js
@@ -9,7 +9,7 @@ export default function TextHeader({ name }) {
   return (
     <SafeAreaHeader>
       <ViewHeader>
-        <Text content={name} weight={700} up={'cap'} size={28} />
+        <Text content={name} weight={600} up={'cap'} size={21} />
       </ViewHeader>
     </SafeAreaHeader>
   )
