81e7f81fef4c103eaa786e8c1165917dc3d5ea03
Manel Rejeb <91155171+Manel-Rejeb@users.noreply.github.com>
2022-07-25 23:16:41 +0200

    working on CablesAndElectricityScreen

--- components/room-service/ElectricityAndCables.screen.js
+++ components/room-service/ElectricityAndCables.screen.js
@@ -1,3 +1,50 @@
+import AreaView from '~/utils/TabAreaView'
+import { Image } from 'styles/image.module'
+import Text from 'components/text/Text'
+import { PhoneDirectoryRow } from 'styles/list.module'
 export default function ElectricityAndCablesScreen() {
-  return <></>
+  return (
+    <AreaView>
+      <Image
+        source={{
+          uri: 'https://goodyfeed.com/wp-content/uploads/2018/10/wall-socket.jpg',
+        }}
+      />
+      <Text
+        size={18}
+        content={
+          'Our hotel room is provided with adapters and cables which are available according to your needs'
+        }
+      />
+      <Text size={18} color={'gray'} content={'Room'} />
+      <PhoneDirectoryRow>
+        <Text
+          size={16}
+          content={'3 Electrical Code for Outlets type (NEC) 120/240-volt'}
+        />
+      </PhoneDirectoryRow>
+      <PhoneDirectoryRow>
+        <Text size={16} content={'4 light swiches '} />
+      </PhoneDirectoryRow>
+      <PhoneDirectoryRow>
+        <Text size={16} content={'5 room lamps '} />
+      </PhoneDirectoryRow>
+
+      <Text size={18} color={'gray'} content={'Bathroom'} />
+      <PhoneDirectoryRow>
+        <Text
+          size={16}
+          content={
+            '1 Electrical Code for Outlets type (NEC)  120-volt receptacle within 3 feet of the outside edge of each sink basin'
+          }
+        />
+      </PhoneDirectoryRow>
+      <PhoneDirectoryRow>
+        <Text size={16} content={'1 light switch'} />
+      </PhoneDirectoryRow>
+      <PhoneDirectoryRow>
+        <Text size={16} content={'1 bathroom light '} />
+      </PhoneDirectoryRow>
+    </AreaView>
+  )
 }
