7254b4dc92fb4442a40c948bdee0e8828b860bbd
walee sebiiy <waleesebiiy@gmail.com>
2022-07-25 22:18:16 +0100

    adding flat list details

--- components/excursions/Excurison.screen.js
+++ components/excursions/Excurison.screen.js
@@ -1,5 +1,68 @@
-import AreaView from 'utils/TabAreaView'
+import NotFound from 'components/notFound/NotFound'
+import FullImageCard from 'components/cards/FullImageCard'
 
-export default function ExcursionScreen() {
-  return <AreaView></AreaView>
+import { useQuery } from 'react-query'
+import { useCallback, useState } from 'react'
+import { __query, baseUrl } from 'hooks/useApi'
+import { RefreshControl, LogBox, FlatList } from 'react-native'
+
+export default function ExcursionScreen({ navigation }) {
+  const { data, refetch, isError, isFetched } = useQuery(
+    '@activities',
+    fetchExcursionsActivities,
+    {
+      initialData: [],
+    }
+  )
+
+  const [refresh, setRefresh] = useState(false)
+
+  let onRefresh = useCallback(() => {
+    setRefresh(true)
+    refetch().then(() => setRefresh(false))
+  }, [])
+
+  return (
+    <>
+      {data.length === 0 ? (
+        <NotFound killProcess={(isFetched && data.length === 0) || isError} />
+      ) : (
+        <FlatList
+          refreshControl={
+            <RefreshControl refreshing={refresh} onRefresh={onRefresh} />
+          }
+          data={data}
+          style={{ margin: 16 }}
+          numColumns={2}
+          horizontal={false}
+          columnWrapperStyle={{ justifyContent: 'space-between' }}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => (
+            <FullImageCard
+              title={item.activity_name}
+              image={`${baseUrl}storage/excursions/thumbnails/${item.activity_image}`}
+              position={'end'}
+              onPress={() =>
+                navigation.navigate('menu-tab-stack-swimming-pool-list', {
+                  _name: item.activity_name,
+                  _data: item,
+                })
+              }
+            />
+          )}
+        />
+      )}
+    </>
+  )
 }
+
+const fetchExcursionsActivities = function () {
+  return __query
+    .get('/api/activities')
+    .then((res) => res.data)
+    .catch((err) => {
+      throw new Error(err.message)
+    })
+}
+
+LogBox.ignoreLogs(['Setting a timer'])
--- hooks/useApi.js
+++ hooks/useApi.js
@@ -1,10 +1,14 @@
 import axios from 'axios'
 
+// export const baseUrl = 'https://smarhotel.herokuapp.com'
+export const baseUrl =
+  'https://c100-2c0f-4280-30-4cf4-38f2-cde5-3e2b-88a2.eu.ngrok.io/'
+
 /**
  * @description create a default instance for axios so we can use it in our components
  * where we won't need to specify the domain url or getting cross origin requests blocked
  */
 export const __query = axios.create({
   withCredentials: true,
-  baseURL: 'https://smarhotel.herokuapp.com',
+  baseURL: baseUrl,
 })
