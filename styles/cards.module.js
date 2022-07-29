0538b9ad62b8ca3c1af98b0d00ba7710fd06acf2
walee sebiiy <waleesebiiy@gmail.com>
2022-07-30 00:47:54 +0100

    styling simple card styles

--- styles/cards.module.js
+++ styles/cards.module.js
@@ -154,21 +154,20 @@ export const StyledSimpleCard = styled.TouchableOpacity`
   width: ${widthPixel(165)}px;
   margin-bottom: ${pixelSizeVertical(14)}px;
   background: ${({ theme }) => theme.primary.accent_0};
-  padding: ${pixelSizeVertical(14)}px ${pixelSizeHorizontal(10)}px;
 `
 
 export const ImageSimpleCard = styled.Image`
   border-radius: 10px;
   width: ${widthPixel(132)}px;
   height: ${heightPixel(132)}px;
+  margin-top: ${pixelSizeVertical(14)}px;
 `
 
 export const TextSimpleCard = styled.View`
   flex: 1;
-  width: 100%;
   align-items: center;
-  justify-content: flex-end;
-  min-height: ${heightPixel(32)}px;
+  justify-content: center;
+  margin: ${pixelSizeVertical(10)}px ${pixelSizeHorizontal(10)}px;
 `
 
 /**
