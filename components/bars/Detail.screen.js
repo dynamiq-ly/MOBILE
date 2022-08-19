ee5324adb80c8fa62c08ca6f676d3442b861fbb2
walee sebiiy <waleesebiiy@gmail.com>
2022-08-19 04:02:24 +0100

    adding bar from database pool

--- components/bars/Detail.screen.js
+++ components/bars/Detail.screen.js
@@ -1,17 +1,26 @@
 import Text from 'components/text/Text'
 import AreaView from 'utils/TabAreaView'
-import Carsouel from 'components/slider/carousel'
-
+import Icon from 'react-native-remix-icon'
 import Button from 'components/button/Button'
+import Carsouel from 'components/slider/carousel'
 import CloseHeader from 'components/header/CloseHeader'
+import TextOverImage from 'components/cards/TextOverImage'
 
+import { useQuery } from 'react-query'
 import { View as Gap } from 'react-native'
+import { baseUrl, __query } from 'hooks/useApi'
 import { useSpring } from '@react-spring/native'
 import { FADE_IN_DONW, FADE_IN_UP } from 'animation/FADE_IN'
-import { View, RadiusView, ButtonWrapperDetail } from 'styles/detail.module'
+import {
+  View,
+  RadiusView,
+  ButtonWrapperDetail,
+  HdRow,
+  HFLine,
+} from 'styles/detail.module'
 
 export default function DetailScreen({ route, navigation }) {
-  const { _data } = route.params
+  const { _id, _data } = route.params
 
   const springContent = useSpring({
     ...FADE_IN_UP.noOpacity,
@@ -21,26 +30,93 @@ export default function DetailScreen({ route, navigation }) {
     ...FADE_IN_DONW.noOpacity,
   })
 
+  const { data } = useQuery(['@bar-details', _id], () => fetchBarById(_id), {
+    initialData: _data,
+    refetchOnMount: true,
+  })
+
   return (
     <View>
       <CloseHeader />
       <Carsouel
         style={{ ...springCarousel }}
-        imageArray={[{ image: _data.image }]}
+        imageArray={[
+          ...data.images.map((el) => ({
+            image: `${baseUrl}storage/bars/${el.image}`,
+          })),
+        ]}
       />
       <RadiusView style={{ ...springContent }}>
-        <AreaView>
+        <AreaView mode={'light'}>
           <Gap style={{ marginBottom: 10 }} />
-          <Text content={_data.name} weight={700} up={'cap'} size={28} />
-          <Text content={_data.content} color={'gray'} weight={400} size={16} />
+          <Text content={data.bar_name} weight={700} up={'cap'} size={28} />
+
+          <HdRow style={{ marginTop: 5 }}>
+            <Icon name={'ri-phone-line'} size={18} style={{ marginRight: 5 }} />
+            <Text content={data.bar_phone_number} />
+          </HdRow>
+          <HdRow style={{ marginTop: 5, marginBottom: 10 }}>
+            <Icon name={'ri-time-line'} size={18} style={{ marginRight: 5 }} />
+            <Text content={data.bar_open_time} />
+          </HdRow>
+          <Text
+            content={data.bar_description}
+            color={'gray'}
+            weight={400}
+            size={16}
+          />
+          <Gap style={{ marginBottom: 10 }} />
+          <TextOverImage
+            image={
+              'https://img.freepik.com/premium-vector/vector-mock-up-white-glass-blank-wine-close-bootle-with-cap-white-label-illustration-realistic-with-shadow-template-design-isolated-dark-background_111984-477.jpg?w=360'
+            }
+            name={'drinks menu'}
+            onPress={() => navigation.navigate('menu-tab-stack-bar-menu-list')}
+          />
+
+          {data.bar_closed_days.length > 0 && (
+            <>
+              <HFLine />
+              <Text content={'closed days'} weight={500} size={18} up={'cap'} />
+              <Gap
+                style={{
+                  flexDirection: 'row',
+                  alignItems: 'center',
+                  marginTop: 10,
+                }}>
+                {data.bar_closed_days.split(',').map((el, key) => (
+                  <Gap
+                    key={key}
+                    style={{
+                      marginRight: 5,
+                      borderRadius: 5,
+                      paddingVertical: 5,
+                      paddingHorizontal: 10,
+                      backgroundColor: '#f87171',
+                    }}>
+                    <Text size={16} content={el} color={'white'} up={'up'} />
+                  </Gap>
+                ))}
+              </Gap>
+            </>
+          )}
+          <Gap style={{ marginBottom: 24 }} />
         </AreaView>
       </RadiusView>
-      <ButtonWrapperDetail>
-        <Button
-          title={'Drinks'}
-          onPress={() => navigation.navigate('menu-tab-stack-bar-menu-list')}
-        />
-      </ButtonWrapperDetail>
+      {data.bar_can_book === 1 && (
+        <ButtonWrapperDetail>
+          <Button title={'reserve your stand'} />
+        </ButtonWrapperDetail>
+      )}
     </View>
   )
+}
+
+let fetchBarById = function (id) {
+  return __query
+    .get(`api/bar/${id}`)
+    .then((res) => res.data)
+    .catch((err) => {
+      throw new Error(err.message)
+    })
 }
--- components/bars/List.screen.js
+++ components/bars/List.screen.js
@@ -1,28 +1,54 @@
 import Widecard from 'components/cards/Widecard'
+import moment from 'moment'
 import NotFound from 'components/notFound/NotFound'
 
-import { bars } from 'mock/bars'
+import { useQuery } from 'react-query'
 import { View } from 'styles/detail.module'
-import { FlatList } from 'react-native'
+import { useCallback, useState } from 'react'
+import { __query, baseUrl } from 'hooks/useApi'
+import { FlatList, LogBox, RefreshControl } from 'react-native'
+
+let time = new Date().getHours() + ':' + new Date().getMinutes()
+let day = moment().format('dddd').toLowerCase()
 
 export default function ListScreen({ navigation }) {
+  const { data, refetch, isFetched, isError } = useQuery('@bar', barsFetcher, {
+    refetchOnMount: true,
+    initialData: [],
+  })
+
+  const [refresh, setRefresh] = useState(false)
+
+  let onRefresh = useCallback(() => {
+    setRefresh(true)
+    refetch().then(() => setRefresh(false))
+  }, [])
+
   return (
     <View>
-      {bars.length === 0 ? (
-        <NotFound />
+      {data.length === 0 ? (
+        <NotFound killProcess={(isFetched && data.length === 0) || isError} />
       ) : (
         <FlatList
-          data={bars}
+          refreshControl={
+            <RefreshControl refreshing={refresh} onRefresh={onRefresh} />
+          }
+          data={data}
           style={{ paddingHorizontal: 14 }}
           keyExtractor={(item) => item.id}
           renderItem={({ item }) => (
             <Widecard
               key={item.id}
-              name={item.name}
-              state={'hidden'}
-              image={item.image}
+              name={day}
+              state={
+                item.bar_open_time < time || item.bar_closed_days.includes(day)
+                  ? false
+                  : true
+              }
+              image={`${baseUrl}storage/bars/${item.images[0].image}`}
               onPress={() =>
                 navigation.navigate('menu-tab-stack-bar-detail', {
+                  _id: item.id,
                   _data: item,
                 })
               }
@@ -33,3 +59,14 @@ export default function ListScreen({ navigation }) {
     </View>
   )
 }
+
+let barsFetcher = function () {
+  return __query
+    .get('api/bar&status=1')
+    .then((res) => res.data)
+    .catch((err) => {
+      throw new Error(err.message)
+    })
+}
+
+LogBox.ignoreAllLogs(true)
