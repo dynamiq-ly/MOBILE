c4e940adbc37ea2d316515c626fd7234fadf6aea
walee sebiiy <waleesebiiy@gmail.com>
2022-09-07 23:24:22 +0100

    adding search animation to search screen

--- screens/src/Search.screen.js
+++ screens/src/Search.screen.js
@@ -1,10 +1,15 @@
-import { Text } from 'components/export'
 import AreaView from 'utils/TabAreaView'
+import LottieView from 'lottie-react-native'
 
 export default function SearchScreen() {
   return (
     <AreaView mode={'dark'}>
-      <Text content={'search'} />
+      <LottieView
+        loop={true}
+        autoPlay={true}
+        style={{ width: '100%' }}
+        source={require('assets/lottie/search_animation.json')}
+      />
     </AreaView>
   )
 }
