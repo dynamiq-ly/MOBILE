9ead3cd1b090f53bbe92e023efb20ed403dacbd7
walee sebiiy <waleesebiiy@gmail.com>
2022-09-24 22:05:36 +0100

    television screen content

--- components/room-service/Television.screen.js
+++ components/room-service/Television.screen.js
@@ -1,7 +1,10 @@
+import Text from 'components/text/Text'
 import AreaView from '~/utils/TabAreaView'
+
 import { Image } from 'styles/image.module'
 import { View as NewView } from 'react-native'
-import Text from 'components/text/Text'
+import { PhoneDirectoryRow } from 'styles/list.module'
+
 export default function Television() {
   return (
     <AreaView>
@@ -14,9 +17,27 @@ export default function Television() {
       <Text
         size={18}
         content={
-          'Each of  our hotel room is provided with a television and a tv remote to keep you entertained in your room and watch your favorite shows and tv programs.'
+          'For your entertainment in the room we offer you a selection of television channels, payed movies and more interactive options.'
         }
+        style={{ marginBottom: 24 }}
       />
+      <PhoneDirectoryRow>
+        <Text content={'You can find the remote next the telivsion.'} />
+      </PhoneDirectoryRow>
+      <PhoneDirectoryRow>
+        <Text
+          content={
+            'The television if automatically connected to our internal wifi.'
+          }
+        />
+      </PhoneDirectoryRow>
+      <PhoneDirectoryRow>
+        <Text
+          content={
+            'We prevent you from using the television system to watch unlicensed or pirated shows.'
+          }
+        />
+      </PhoneDirectoryRow>
     </AreaView>
   )
 }
