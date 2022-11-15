20c49dd0ff93287a2656767c0c9d15975c0fd18f
Manel Rejeb <91155171+Manel-Rejeb@users.noreply.github.com>
2022-11-15 16:10:33 +0100

    WeatherScreen Design

--- screens/src/Bookmark.screen.js
+++ screens/src/Bookmark.screen.js
@@ -1,10 +1,5 @@
-import { Text } from 'components/export'
 import AreaView from 'utils/TabAreaView'
 
 export default function BookmarkScreen() {
-  return (
-    <AreaView mode={'dark'}>
-      <Text content={'bookmarks'} />
-    </AreaView>
-  )
+  return <AreaView mode={'dark'}></AreaView>
 }
