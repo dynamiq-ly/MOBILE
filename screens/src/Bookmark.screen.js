0aa274c4439c110a3330d834dd098a355f5b8743
walee sebiiy <waleesebiiy@gmail.com>
2022-11-19 10:25:06 +0100

    bookmark styled add

--- screens/src/Bookmark.screen.js
+++ screens/src/Bookmark.screen.js
@@ -3,8 +3,9 @@ import {
   StyledBookMark,
   StyledGalleryPhoto,
   StyledGrid,
+  StyledThmubnailOverlay,
   StyledThumbnail,
-} from '~/styles/bookMark.module'
+} from 'styles/bookMark.module'
 import Text from 'components/text/Text'
 export default function BookmarkScreen() {
   return (
@@ -12,14 +13,18 @@ export default function BookmarkScreen() {
       <StyledGrid>
         <StyledBookMark>
           <StyledGalleryPhoto>
-            {[...Array(3)].map((_, key) => (
-              <StyledThumbnail
-                key={key}
-                source={{
-                  uri: 'https://vastphotos.com/files/uploads/assets/ubermenu_thumb_landscapes.jpg',
-                }}
-              />
-            ))}
+            {Object.keys(bookmarked_data)
+              .map((el) => bookmarked_data[el])
+              .flat(1)
+              .map((el, key) => (
+                <StyledThumbnail
+                  key={key}
+                  source={{
+                    uri: el.image,
+                  }}>
+                  <StyledThmubnailOverlay />
+                </StyledThumbnail>
+              ))}
           </StyledGalleryPhoto>
           <Text
             align={'center'}
@@ -30,47 +35,56 @@ export default function BookmarkScreen() {
             style={{ marginTop: 8 }}
           />
         </StyledBookMark>
-        <StyledBookMark>
-          <StyledGalleryPhoto>
-            {[...Array(3)].map((_, key) => (
-              <StyledThumbnail
-                key={key}
-                source={{
-                  uri: 'https://vastphotos.com/files/uploads/assets/ubermenu_thumb_landscapes.jpg',
-                }}
-              />
-            ))}
-          </StyledGalleryPhoto>
-          <Text
-            align={'center'}
-            content={'point of interest'}
-            up={'up'}
-            size={18}
-            weight={600}
-            style={{ marginTop: 8 }}
-          />
-        </StyledBookMark>
-        <StyledBookMark>
-          <StyledGalleryPhoto>
-            {[...Array(3)].map((_, key) => (
-              <StyledThumbnail
-                key={key}
-                source={{
-                  uri: 'https://vastphotos.com/files/uploads/assets/ubermenu_thumb_landscapes.jpg',
-                }}
-              />
-            ))}
-          </StyledGalleryPhoto>
-          <Text
-            align={'center'}
-            content={'entertainment'}
-            up={'up'}
-            size={18}
-            weight={600}
-            style={{ marginTop: 8 }}
-          />
-        </StyledBookMark>
+        {Object.keys(bookmarked_data).map((el, index) => (
+          <StyledBookMark key={index}>
+            <StyledGalleryPhoto>
+              {bookmarked_data[el].map((elem, key) => (
+                <StyledThumbnail
+                  key={key}
+                  source={{
+                    uri: elem.image,
+                  }}>
+                  <StyledThmubnailOverlay />
+                </StyledThumbnail>
+              ))}
+            </StyledGalleryPhoto>
+            <Text
+              align={'center'}
+              content={el.replaceAll('_', ' ')}
+              up={'up'}
+              size={18}
+              weight={600}
+              style={{ marginTop: 8 }}
+            />
+          </StyledBookMark>
+        ))}
       </StyledGrid>
     </AreaView>
   )
+}
+
+const bookmarked_data = {
+  point_of_interest: [
+    {
+      image:
+        'https://carthagemagazine.com/wp-content/uploads/2021/10/Grand-Port-of-Carthage.jpg',
+    },
+    {
+      image: 'https://p6.storage.canalblog.com/66/96/525997/67513186.jpg',
+    },
+    {
+      image:
+        'https://www.tunisiatourism.info/uploads/editor/67933562-1600278583.jpg',
+    },
+  ],
+
+  excursions: [
+    {
+      image: 'https://media.www.kent.ac.uk/se/18831/iStock-503925194_2000w.jpg',
+    },
+    {
+      image:
+        'https://upload.wikimedia.org/wikipedia/commons/8/80/Dressage_Arabian_%282938612678%29.jpg',
+    },
+  ],
 }
--- styles/bookMark.module.js
+++ styles/bookMark.module.js
@@ -1,30 +1,38 @@
 import styled from 'styled-components'
 
 export const StyledGrid = styled.View`
-  flex-direction: row;
   flex-wrap: wrap;
+  margin-top: 14px;
+  flex-direction: row;
   justify-content: space-between;
 `
 
 export const StyledBookMark = styled.View`
-  width: 160px;
+  width: 170px;
   margin-bottom: 24px;
   align-item: center;
   justify-content: center;
 `
 
 export const StyledGalleryPhoto = styled.TouchableOpacity`
-  width: 160px;
-  height: 160px;
-  max-height: 160px;
+  width: 170px;
+  height: 170px;
+  flex-wrap: wrap;
+  overflow: hidden;
+  max-height: 170px;
   border-radius: 12px;
   position: relative;
-  overflow: hidden;
-  flex-wrap: wrap;
-  background: ${({ theme }) => theme.secondary.accent_100};
+  flex-direction: row;
+  background: ${({ theme }) => `${theme.secondary.accent_700}15`};
 `
 
 export const StyledThumbnail = styled.ImageBackground`
-  height: 80px;
-  width: 80px;
+  height: 85px;
+  width: 85px;
+`
+
+export const StyledThmubnailOverlay = styled.View`
+  width: 100%;
+  height: 100%;
+  background: ${({ theme }) => `${theme.primary.accent_900}40`};
 `
