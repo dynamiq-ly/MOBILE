b7247d5409bb45600350e6fbb6ffb96441e8227e
walee sebiiy <waleesebiiy@gmail.com>
2022-12-23 10:23:12 +0100

    drink service menu cateogry

--- components/room-service/DrinksService.screen.js
+++ components/room-service/DrinksService.screen.js
@@ -1,8 +1,7 @@
-import AreaView from 'utils/TabAreaView'
 import Text from 'components/text/Text'
+import AreaView from 'utils/TabAreaView'
 import SimpleCard from 'components/cards/SimpleCard'
 
-import { useState } from 'react'
 import { GridLayout } from 'styles/grid.module'
 
 import {
@@ -11,17 +10,40 @@ import {
   VerticalListLine,
 } from 'styles/list.module'
 
-const DrinksServiceScreen = () => {
+import { useQuery } from 'react-query'
+import { useCallback, useState } from 'react'
+import { __query, baseUrl } from 'hooks/useApi'
+import { LogBox, RefreshControl } from 'react-native'
+
+export default function DrinksServiceScreen({ navigation }) {
   const [isCategory, setCategory] = useState('SOFT')
 
+  const { data, refetch } = useQuery(
+    '@room-service-drink-menu',
+    drinkServiceFetcher,
+    {
+      refetchOnMount: true,
+    }
+  )
+
+  const [refresh, setRefresh] = useState(false)
+
+  let onRefresh = useCallback(() => {
+    setRefresh(true)
+    refetch().then(() => setRefresh(false))
+  }, [])
+
   return (
-    <AreaView>
+    <AreaView
+      refreshControl={
+        <RefreshControl refreshing={refresh} onRefresh={onRefresh} />
+      }>
       <Text
+        size={16}
         content={
-          'Order your drinks from here and enjoy a refreshing beverage at your door step.'
+          'Order your drinks from here and enjoy a refreshing beverage with your meal.'
         }
       />
-
       <VerticalList>
         <VerticalListItem onPress={() => setCategory('SOFT')}>
           <Text
@@ -43,17 +65,47 @@ const DrinksServiceScreen = () => {
           />
         </VerticalListItem>
       </VerticalList>
-
       <GridLayout>
-        <SimpleCard
-          title={'hello'}
-          image={
-            'https://images.immediate.co.uk/production/volatile/sites/30/2013/05/Singapore-sling-7ddad3e.jpg'
-          }
-        />
+        {data &&
+          data
+            .filter((el) => el.drink_drink_type === isCategory)
+            .map((el, key) => {
+              return (
+                <SimpleCard
+                  key={key}
+                  title={el.drink_drink_category}
+                  image={`${baseUrl}storage/room-service/drink-service/${el.drink_drink_image}`}
+                  onPress={() =>
+                    navigation.navigate(
+                      el.drink_drink_type === 'ALCOHOL'
+                        ? 'menu-tab-stack-restaurant-detail-menu-drinks-details-alcohol'
+                        : 'menu-tab-stack-restaurant-detail-menu-drinks-details',
+                      {
+                        _id: el.id,
+                        _data:
+                          el.drink_drink_type === 'ALCOHOL'
+                            ? el.alcoholdrinks
+                            : el.softdrinks,
+                        _name: el.drink_drink_category,
+                        _image: `${baseUrl}storage/restaurants/menu/drink/thumbnails/${el.drink_drink_image}`,
+                      }
+                    )
+                  }
+                />
+              )
+            })}
       </GridLayout>
     </AreaView>
   )
 }
 
-export default DrinksServiceScreen
+let drinkServiceFetcher = function (id) {
+  return __query
+    .get('api/room-service/drink-service/category')
+    .then((res) => res.data)
+    .catch((err) => {
+      throw new Error(err.message)
+    })
+}
+
+LogBox.ignoreAllLogs(true)
