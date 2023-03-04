fa2cd3b8f13b9f2cde1119d51e39e4af6482aae6
walee sebiiy <waleesebiiy@gmail.com>
2023-03-04 11:51:42 +0100

    tv data fetcher

--- components/room-service/Television.screen.js
+++ components/room-service/Television.screen.js
@@ -5,39 +5,50 @@ import { Image } from 'styles/image.module'
 import { View as NewView } from 'react-native'
 import { PhoneDirectoryRow } from 'styles/list.module'
 
+import { __query } from 'hooks/useApi'
+import { useQuery } from 'react-query'
+import { useCallback, useState } from 'react'
+import { LogBox, RefreshControl } from 'react-native'
+
 export default function Television() {
+  const { data, refetch, isFetched, isError } = useQuery('@towels', televisionInstructionFetcher, {
+    refetchOnMount: true,
+    initialData: [],
+  })
+
+  const [refresh, setRefresh] = useState(false)
+
+  let onRefresh = useCallback(() => {
+    setRefresh(true)
+    refetch().then(() => setRefresh(false))
+  }, [])
+
   return (
-    <AreaView>
+    <AreaView refreshControl={<RefreshControl refreshing={refresh} onRefresh={onRefresh} />}>
       <Image
         source={{
           uri: 'https://cdn.mos.cms.futurecdn.net/chdRXnwnNQTmg9Ax4EvmBb-1024-80.jpg.webp',
         }}
       />
       <NewView style={{ marginBottom: 15 }} />
-      <Text
-        size={18}
-        content={
-          'For your entertainment in the room we offer you a selection of television channels, payed movies and more interactive options.'
-        }
-        style={{ marginBottom: 24 }}
-      />
-      <PhoneDirectoryRow>
-        <Text content={'You can find the remote next the telivsion.'} />
-      </PhoneDirectoryRow>
-      <PhoneDirectoryRow>
-        <Text
-          content={
-            'The television if automatically connected to our internal wifi.'
-          }
-        />
-      </PhoneDirectoryRow>
-      <PhoneDirectoryRow>
-        <Text
-          content={
-            'We prevent you from using the television system to watch unlicensed or pirated shows.'
-          }
-        />
-      </PhoneDirectoryRow>
+      <Text size={18} content={'For your entertainment in the room we offer you a selection of television channels, payed movies and more interactive options.'} style={{ marginBottom: 24 }} />
+
+      {data.map((el) => (
+        <PhoneDirectoryRow key={el.id}>
+          <Text content={el.tvInstruction} />
+        </PhoneDirectoryRow>
+      ))}
     </AreaView>
   )
 }
+
+let televisionInstructionFetcher = function () {
+  return __query
+    .get('api/television')
+    .then((res) => res.data)
+    .catch((err) => {
+      throw new Error(err.message)
+    })
+}
+
+LogBox.ignoreAllLogs(true)
