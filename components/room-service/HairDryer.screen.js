be5b0ac5cd397d3cf84b7599ed31b191e4cf0082
walee sebiiy <waleesebiiy@gmail.com>
2023-03-04 12:33:07 +0100

    hair drayer

--- components/room-service/HairDryer.screen.js
+++ components/room-service/HairDryer.screen.js
@@ -6,84 +6,57 @@ import Text from 'components/text/Text'
 import { BoxIcon, BoxText, PhoneDirectoryRow } from 'styles/list.module'
 import { fontPixel } from 'utils/normalization'
 
+import { __query } from 'hooks/useApi'
+import { useQuery } from 'react-query'
+import { useCallback, useState } from 'react'
+import { LogBox, RefreshControl } from 'react-native'
+
 export default function HairDryer() {
+  const { data, refetch, isFetched, isError } = useQuery('@hairDrayer', hairDrayerInstructionFetcher, {
+    refetchOnMount: true,
+    initialData: [],
+  })
+
+  const [refresh, setRefresh] = useState(false)
+
+  let onRefresh = useCallback(() => {
+    setRefresh(true)
+    refetch().then(() => setRefresh(false))
+  }, [])
+
   return (
-    <AreaView>
+    <AreaView refreshControl={<RefreshControl refreshing={refresh} onRefresh={onRefresh} />}>
       <Image
         source={{
           uri: 'https://i.insider.com/5983507cefe3df2b008b45ef?width=1000&format=jpeg&auto=webp',
         }}
       />
       <NewView style={{ marginBottom: 15 }} />
-      <Text
-        size={18}
-        content={
-          'Our hotel room is provided with a hair dryer in the bathroom of each hotrl room.'
-        }
-      />
+      <Text size={18} content={'Our hotel room is provided with a hair dryer in the bathroom of each hotrl room.'} />
       <Text size={18} color={'gray'} content={'How to use '} />
-      <PhoneDirectoryRow>
-        <Text
-          size={16}
-          content={
-            '1. Adjust the desired blowing strength:                                         Position 0: OFF                                                                   Position 1: Half power, medium temperature for gentle drying and styling                                                     Position 2: Full power, high temperature for quick drying'
-          }
-        />
-      </PhoneDirectoryRow>
-      <PhoneDirectoryRow>
-        <Text
-          size={16}
-          content={
-            '2. Use the high setting for drying hair, and use low setting for styling.'
-          }
-        />
-      </PhoneDirectoryRow>
 
-      <PhoneDirectoryRow>
-        <Text
-          size={16}
-          content={
-            '3. Should the hair dryer stop working for any reason, turn it off at once and let it cool down.'
-          }
-        />
-      </PhoneDirectoryRow>
-      <PhoneDirectoryRow>
-        <Text
-          size={16}
-          content={
-            '4. Always ensure the switch is set to OFF position before plugging the unit in the power outlet.                                                                               Cool shot: with this switch'
-          }
-        />
-      </PhoneDirectoryRow>
-      <Text size={18} color={'gray'} content={'Safety instructions'} />
-      <PhoneDirectoryRow>
-        <Text
-          size={16}
-          content={
-            'The applicable voltage is 220-240V, ~50/60Hz. For safety reasons it is not appropriate to connect multiple devices to one power outlet. '
-          }
-        />
-      </PhoneDirectoryRow>
-
-      <PhoneDirectoryRow>
-        <BoxIcon>
-          <Icon
-            name={'ri-error-warning-line'}
-            size={fontPixel(21)}
-            color={'orange'}
-          />
-        </BoxIcon>
-        <BoxText>
-          <Text
-            content={
-              'This device may be used by children over 8 years of age and persons with reduced physical, sensory or mental capabilities. Never put the power cable, the plug or the whole device into the water.                                                                 Never use the product in humid conditions.'
-            }
-            color={'gray'}
-            size={14}
-          />
-        </BoxText>
-      </PhoneDirectoryRow>
+      {data.map((el) => (
+        <PhoneDirectoryRow key={el.id} style={{ alignItems: 'center' }}>
+          <BoxIcon>
+            <Icon name={el.HairDryerWarning ? 'ri-error-warning-line' : 'ri-arrow-right-circle-fill'} color={el.HairDryerWarning ? '#EE4231' : '#212121'} />
+          </BoxIcon>
+          <BoxText>
+            <Text color={'gray'} size={17} content={el.HairDryerInstruction} />
+          </BoxText>
+        </PhoneDirectoryRow>
+      ))}
       <NewView style={{ marginBottom: 24 }} />
     </AreaView>
   )
 }
+
+let hairDrayerInstructionFetcher = function () {
+  return __query
+    .get('api/hair-dryer')
+    .then((res) => res.data)
+    .catch((err) => {
+      throw new Error(err.message)
+    })
+}
+
+LogBox.ignoreAllLogs(true)
