fcd8fb7d07b305fd2b606ca1ef69b83f03f3e911
walee sebiiy <waleesebiiy@gmail.com>
2023-03-04 12:12:23 +0100

    air condition fetcher

--- components/room-service/AirConditioner.screen.js
+++ components/room-service/AirConditioner.screen.js
@@ -6,58 +6,58 @@ import { Image } from 'styles/image.module'
 import { BoxIcon, BoxText, PhoneDirectoryRow } from 'styles/list.module'
 import { View as NewView } from 'react-native'
 
+import { __query } from 'hooks/useApi'
+import { useQuery } from 'react-query'
+import { useCallback, useState } from 'react'
+import { LogBox, RefreshControl } from 'react-native'
+
 export default function AirConditioner() {
+  const { data, refetch, isFetched, isError } = useQuery('@airCondition', airConditionInstructionFetcher, {
+    refetchOnMount: true,
+    initialData: [],
+  })
+
+  const [refresh, setRefresh] = useState(false)
+
+  let onRefresh = useCallback(() => {
+    setRefresh(true)
+    refetch().then(() => setRefresh(false))
+  }, [])
+
   return (
-    <AreaView>
+    <AreaView refreshControl={<RefreshControl refreshing={refresh} onRefresh={onRefresh} />}>
       <Image
         source={{
           uri: 'https://www.phillyaptrentals.com/wp-content/uploads/2020/04/Central-Air-Conditioning-scaled.jpg',
         }}
       />
       <NewView style={{ marginBottom: 15 }} />
-      <Text
-        size={18}
-        content={
-          'Our hotel room is provided with an AC for temperature control in your room.'
-        }
-      />
+      <Text size={18} content={'Our hotel room is provided with an AC for temperature control in your room.'} />
       <NewView style={{ marginBottom: 15 }} />
-      <Text
-        size={18}
-        up={'up'}
-        weight={600}
-        content={'Air Conditioner Remote Controls & AC Settings: '}
-      />
+      <Text size={18} up={'up'} weight={600} content={'Air Conditioner Remote Controls & AC Settings: '} />
       <NewView style={{ marginBottom: 15 }} />
-      <PhoneDirectoryRow>
-        <Text
-          color={'gray'}
-          content={
-            'The switch is located on the head of the bed. It has three speeds (1 = strong, 2 = normal, 3 = soft)'
-          }
-        />
-      </PhoneDirectoryRow>
-      <PhoneDirectoryRow>
-        <Text
-          size={16}
-          color={'gray'}
-          content={'Thermostat at the entrance of the bedroom.'}
-        />
-      </PhoneDirectoryRow>
-      <PhoneDirectoryRow>
-        <BoxIcon>
-          <Icon name={'ri-error-warning-line'} color={'orange'} />
-        </BoxIcon>
-        <BoxText>
-          <Text
-            size={16}
-            color={'gray'}
-            content={
-              'When you open the terrace door, the air conditioning will switch off automatically.'
-            }
-          />
-        </BoxText>
-      </PhoneDirectoryRow>
+
+      {data.map((el) => (
+        <PhoneDirectoryRow key={el.id} style={{ alignItems: 'center' }}>
+          <BoxIcon>
+            <Icon name={el.AirConditionnerWarning ? 'ri-error-warning-line' : 'ri-arrow-right-circle-fill'} color={el.AirConditionnerWarning ? '#EE4231' : '#212121'} />
+          </BoxIcon>
+          <BoxText>
+            <Text color={'gray'} size={17} content={el.AirConditionnerInstruction} />
+          </BoxText>
+        </PhoneDirectoryRow>
+      ))}
     </AreaView>
   )
 }
+
+let airConditionInstructionFetcher = function () {
+  return __query
+    .get('api/air-conditionner')
+    .then((res) => res.data)
+    .catch((err) => {
+      throw new Error(err.message)
+    })
+}
+
+LogBox.ignoreAllLogs(true)
--- components/room-service/ElectricityAndCables.screen.js
+++ components/room-service/ElectricityAndCables.screen.js
@@ -7,26 +7,35 @@ import { Image } from 'styles/image.module'
 import { TextArea } from 'components/input/Input'
 import { PhoneDirectoryRow } from 'styles/list.module'
 
+import { __query } from 'hooks/useApi'
+import { useQuery } from 'react-query'
+import { useCallback, useState } from 'react'
+import { LogBox, RefreshControl } from 'react-native'
+
 export default function ElectricityAndCablesScreen() {
+  const { data, refetch, isFetched, isError } = useQuery('@electricity', electricityInstructionFetcher, {
+    refetchOnMount: true,
+    initialData: [],
+  })
+
+  const [refresh, setRefresh] = useState(false)
+
+  let onRefresh = useCallback(() => {
+    setRefresh(true)
+    refetch().then(() => setRefresh(false))
+  }, [])
+
   return (
-    <AreaView>
+    <AreaView refreshControl={<RefreshControl refreshing={refresh} onRefresh={onRefresh} />}>
       <Image
         source={{
           uri: 'https://goodyfeed.com/wp-content/uploads/2018/10/wall-socket.jpg',
         }}
       />
-      <Text
-        size={18}
-        content={
-          'Our hotel room is provided with adapters and cables which are available according to your needs'
-        }
-      />
+      <Text size={18} content={'Our hotel room is provided with adapters and cables which are available according to your needs'} />
       <Text size={18} color={'gray'} content={'Room'} />
       <PhoneDirectoryRow>
-        <Text
-          size={16}
-          content={'3 Electrical Code for Outlets type (NEC) 120/240-volt'}
-        />
+        <Text size={16} content={'3 Electrical Code for Outlets type (NEC) 120/240-volt'} />
       </PhoneDirectoryRow>
       <PhoneDirectoryRow>
         <Text size={16} content={'4 light swiches '} />
@@ -37,12 +46,7 @@ export default function ElectricityAndCablesScreen() {
 
       <Text size={18} color={'gray'} content={'Bathroom'} />
       <PhoneDirectoryRow>
-        <Text
-          size={16}
-          content={
-            '1 Electrical Code for Outlets type (NEC)  120-volt receptacle within 3 feet of the outside edge of each sink basin'
-          }
-        />
+        <Text size={16} content={'1 Electrical Code for Outlets type (NEC)  120-volt receptacle within 3 feet of the outside edge of each sink basin'} />
       </PhoneDirectoryRow>
       <PhoneDirectoryRow>
         <Text size={16} content={'1 light switch'} />
@@ -56,3 +60,14 @@ export default function ElectricityAndCablesScreen() {
     </AreaView>
   )
 }
+
+let electricityInstructionFetcher = function () {
+  return __query
+    .get('api/electricity')
+    .then((res) => res.data)
+    .catch((err) => {
+      throw new Error(err.message)
+    })
+}
+
+LogBox.ignoreAllLogs(true)
