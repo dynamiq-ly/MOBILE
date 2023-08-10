620496a8030f0de4baeac478efd7f079da04b9ba
Manel Rejeb <91155171+Manel-Rejeb@users.noreply.github.com>
2023-08-10 19:48:37 +0200

    adding data for day activities in entertainement

--- mocks/entertainment.data.js
+++ mocks/entertainment.data.js
@@ -2,8 +2,7 @@ export const data = {
   id: '[stack] stack-entertainment',
   image: 'https://kammakshi.com/images/work-3.jpg',
   title: 'Entertainements',
-  description:
-    'Lorem ipsum dolor sit amet, consectetur adipiscing sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Enim ad minim veniam, ullamco laboris commodo consequat...Read More',
+  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Enim ad minim veniam, ullamco laboris commodo consequat...Read More',
   staff: [
     {
       name: 'John Doe',
@@ -65,8 +64,7 @@ export const data = {
   day_activities: [
     {
       id: 1,
-      image:
-        'https://www.dickssportinggoods.com/protips/sports-and-activities/family-fun/top-10-pool-party-must-haves/_jcr_content/root/container/container_2/image.coreimg.jpeg/1667591152305/poolpartyessentials.jpeg',
+      image: 'https://www.dickssportinggoods.com/protips/sports-and-activities/family-fun/top-10-pool-party-must-haves/_jcr_content/root/container/container_2/image.coreimg.jpeg/1667591152305/poolpartyessentials.jpeg',
       title: 'Pool Party',
       description: 'enjoying your time at the pool with our animation programmer',
       duration: '1h',
@@ -75,6 +73,42 @@ export const data = {
       type: 'Family',
       join: true,
       time_table: ['Tuesday, 14 june - 15:00', 'Wednesday, 15 june - 15:00', 'Thursday, 16 june - 15:00'],
+    },
+    {
+      id: 2,
+      image: 'https://cdn.britannica.com/94/125794-050-FB09B3F4/Hikers-Gore-Range-Mountains-Denver.jpg',
+      title: 'Hiking Adventure',
+      description: 'Embark on an exciting hiking adventure with our experienced guides. Explore breathtaking trails and enjoy the beauty of nature.',
+      duration: '3h',
+      time: '09:00 - 12:00',
+      location: 'Trailhead',
+      type: 'Adult',
+      join: true,
+      time_table: ['Saturday, 20 August - 09:00', 'Sunday, 21 August - 09:00', 'Monday, 22 August - 09:00'],
+    },
+    {
+      id: 3,
+      image: 'https://woodstockschoolofart.org/wp-content/uploads/2022/12/PXL_20221108_193644346-scaled.jpg',
+      title: 'Art Workshop',
+      description: 'Unleash your creativity in our interactive art workshop. Learn new techniques and create your own masterpiece with the guidance of professional artists.',
+      duration: '2h',
+      time: '14:00 - 16:00',
+      location: 'Art Studio',
+      type: 'Teen',
+      join: true,
+      time_table: ['Friday, 26 August - 14:00', 'Saturday, 27 August - 14:00', 'Sunday, 28 August - 14:00'],
+    },
+    {
+      id: 4,
+      image: 'https://oppq.qc.ca/wp-content/uploads/Yoga-portrait-des-avantages.jpg',
+      title: 'Yoga Retreat',
+      description: 'Relax and rejuvenate your body and mind in our serene yoga retreat. Experience calming yoga sessions and meditation amidst a tranquil setting.',
+      duration: '1.5h',
+      time: '16:30 - 18:00',
+      location: 'Yoga Pavilion',
+      type: 'Adult',
+      join: true,
+      time_table: ['Wednesday, 10 August - 16:30', 'Thursday, 11 August - 16:30', 'Friday, 12 August - 16:30'],
     },
   ],
 }
