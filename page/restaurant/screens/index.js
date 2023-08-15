02de40351af1bd128d60e7afb5a7659ca8f6ea6a
Manel Rejeb <91155171+Manel-Rejeb@users.noreply.github.com>
2023-08-16 00:56:07 +0200

    create restaurants details screen routing in index

--- page/restaurant/screens/index.js
+++ page/restaurant/screens/index.js
@@ -1,2 +1,3 @@
 export { default as Restaurant } from '@/page/restaurant/screens/restaurant'
 export { default as RestaurantDetail } from '@/page/restaurant/screens/restaurant-details'
+export { default as RestaurantDetailMenu } from '@/page/restaurant/screens/restaurant-details-menu'
