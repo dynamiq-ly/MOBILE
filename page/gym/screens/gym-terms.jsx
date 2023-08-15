78f41eb153e8329bb93ce628b2d3507cb4027c47
Walee Sebiiy <waleesebiiy@gmail.com>
2023-08-15 21:47:56 +0100

    [207] gym feature

--- page/gym/screens/gym-terms.jsx
+++ page/gym/screens/gym-terms.jsx
@@ -1,8 +1,9 @@
 /* packages */
 import { WebView } from 'react-native-webview'
+import { Platform } from 'react-native'
 
 export default ({ route }) => {
   const { data } = route.params
 
-  return <WebView style={{ flex: 1 }} source={{ uri: data || 'https://landing.utells.com/' }} />
+  return <WebView style={{ flex: 1 }} source={{ uri: Platform.OS === 'ios' ? data : `https://docs.google.com/gview?embedded=true&url=${data}` || 'https://landing.utells.com/' }} />
 }
