0d22c74504349719c5c3f39fa49d444f15eacd4b
Manel Rejeb <91155171+Manel-Rejeb@users.noreply.github.com>
2023-08-16 00:54:09 +0200

    create restaurant menu details list routing

--- page/restaurant/routes.js
+++ page/restaurant/routes.js
@@ -3,7 +3,7 @@ import { Icon } from '@/components'
 import { StackHeader, OptionHeader } from '@/shared'
 
 /* screens */
-import { Restaurant, RestaurantDetail } from '@/page/restaurant/screens'
+import { Restaurant, RestaurantDetail, RestaurantDetailMenu } from '@/page/restaurant/screens'
 
 export const RestaurantRoutesConfig = [
   (initial = {
@@ -22,6 +22,15 @@ export const RestaurantRoutesConfig = [
       headerShown: true,
       headerTitle: 'Restaurant Detail',
       header: () => <OptionHeader />,
+    },
+  }),
+  (detailMenu = {
+    id: '[stack] stack-restaurant-detail-menu',
+    component: RestaurantDetailMenu,
+    option: {
+      headerShown: true,
+      headerTitle: 'Restaurant Menu',
+      header: ({ route }) => <StackHeader title={`${route.params.name} Menu` || 'Drinks Menu'} iconLeft={<Icon icon={require('@/assets/icons/product/monocrome/arrow-back-dark.png')} size={18} />} />,
     },
   }),
 ]
