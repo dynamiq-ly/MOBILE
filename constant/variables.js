1132db3edcbff92d8ef203f1ce48cea663c11c53
Walee Sebiiy <waleesebiiy@gmail.com>
2023-08-15 10:36:59 +0100

    [US-202] -add api calls to the feature

--- constant/variables.js
+++ constant/variables.js
@@ -1,5 +1,6 @@
 export const variables = {
   APP_NAME: 'utells',
   API_URL: 'https://api.utells.com',
+  STORAGE_LINK: 'https://api.utells.com/storage',
   WEATHER_API_KEY: '06c0104d180141e2a99121002231208',
 }
--- page/policies/screens/hotel/hotel-policies-details.jsx
+++ page/policies/screens/hotel/hotel-policies-details.jsx
@@ -1,8 +1,19 @@
 /* packages */
 import { WebView } from 'react-native-webview'
+import { Platform } from 'react-native'
+
+/* styles */
+import { useTheme } from 'styled-components'
 
 export default ({ route }) => {
   const { file } = route.params
 
-  return <WebView style={{ flex: 1 }} source={{ uri: file || 'https://landing.utells.com/' }} />
+  const theme = useTheme()
+
+  return (
+    <WebView
+      style={{ flex: 1, backgroundColor: theme.core.background['variant_view'] }}
+      source={{ uri: Platform.OS === 'ios' ? file : `https://docs.google.com/gview?embedded=true&url=${file}` || 'https://landing.utells.com/' }}
+    />
+  )
 }
