d2123748ca7c650dceaed1bc1b81a433d8bd2a93
Manel Rejeb <91155171+Manel-Rejeb@users.noreply.github.com>
2023-08-17 12:16:18 +0200

    creating routing routing in the index

--- page/restaurant/screens/index.js
+++ page/restaurant/screens/index.js
@@ -1,3 +1,4 @@
 export { default as Restaurant } from '@/page/restaurant/screens/restaurant'
 export { default as RestaurantDetail } from '@/page/restaurant/screens/restaurant-details'
 export { default as RestaurantDetailMenu } from '@/page/restaurant/screens/restaurant-details-menu'
+export { default as RestaurantDetailMenuList } from '@/page/restaurant/screens/restaurant-details-menu-list'
