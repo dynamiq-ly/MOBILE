b602f9b5ae1da20b41d501bdf9416f87d7d04e63
Walee Sebiiy <waleesebiiy@gmail.com>
2023-08-19 18:25:22 +0100

    pushing restaurant features

--- page/bar/screens/bar-details.jsx
+++ page/bar/screens/bar-details.jsx
@@ -1,5 +1,5 @@
 /* packages */
-import { TouchableOpacity, View } from 'react-native'
+import { RefreshControl, TouchableOpacity, View } from 'react-native'
 import { useCallback, useState } from 'react'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
 import Animated, { useAnimatedStyle, useSharedValue, withTiming } from 'react-native-reanimated'
@@ -15,10 +15,14 @@ import { Icon, Pager } from '@/components'
 import { Container } from '@/shared'
 import { BottomSheet } from '@/components'
 import { Button, Div, Image, Text } from '@/common'
-4
+import { variables } from '@/constant/variables'
+
+/* mocks */
+import { useFetch } from '@/hook/useFetch'
+import { useQuery } from 'react-query'
 
 export default ({ navigation, route }) => {
-  const { data } = route.params
+  const { id, data: bar } = route.params
 
   const theme = useTheme()
   const { top } = useSafeAreaInsets()
@@ -38,10 +42,26 @@ export default ({ navigation, route }) => {
     index === 3 ? (paddingTop.value = top) : (paddingTop.value = theme.units['md'])
   }, [])
 
+  /* data fetching */
+  const [refresh, setRefresh] = useState(false)
+
+  const { data, isLoading, error, refetch } = useQuery(['@Bars-Detail', id], () => getClientSideQueries.getBarById(id), {
+    initialData: bar,
+  })
+
+  let onRefresh = useCallback(() => {
+    setRefresh(true)
+    refetch().then(() => setRefresh(false))
+  }, [])
+
+  if (isLoading) return <Text>Loading...</Text>
+
+  if (error) return <Text>Error: {error.message}</Text>
+
   return (
     <View style={{ flex: 1 }}>
-      <Container stickyHeaderIndices={[0]} padding={false} safeArea={false}>
-        <Pager slider={data.images} interval={5000} />
+      <Container stickyHeaderIndices={[0]} padding={false} safeArea={false} refreshControl={<RefreshControl refreshing={refresh} onRefresh={onRefresh} />}>
+        <Pager slider={data.images.map((el) => ({ ...el, image: `${variables.STORAGE_LINK}/bars/${el.image}` }))} interval={5000} />
 
         {/* title */}
         <View style={{ paddingHorizontal: theme.units.md }}>
@@ -55,7 +75,7 @@ export default ({ navigation, route }) => {
         <View style={{ gap: theme.units.sm, paddingHorizontal: theme.units.md }}>
           <View style={{ flexDirection: 'row', alignItems: 'center', gap: theme.units.sm }}>
             <Image source={require('@/assets/icons/product/monocrome/clock-dark.png')} height='14px' width='14px' />
-            <Text size={6} weight='md'>{`From ${data.timing.open} to ${data.timing.close}`}</Text>
+            <Text size={6} weight='md'>{`From ${data.timing_open} to ${data.timing_close}`}</Text>
           </View>
           <View style={{ flexDirection: 'row', alignItems: 'center', gap: theme.units.sm }}>
             <Image source={require('@/assets/icons/product/monocrome/phone-dark.png')} height='14px' width='14px' />
@@ -82,51 +102,53 @@ export default ({ navigation, route }) => {
         </TouchableOpacity>
 
         {/* staff */}
-        <View style={{ paddingHorizontal: theme.units.md }}>
-          <Div filled title='staff' radii='md'>
-            <BottomSheet
-              handleSheetChange={handleSheetChanges}
-              triggerElement={
-                <View style={{ position: 'relative' }}>
-                  <Div>
-                    <View style={{ flexDirection: 'row', alignItems: 'center', gap: theme.units.sb }}>
-                      <Image source={data.staff[0].image} height='32px' width='32px' radii='rounded' />
-                      <View>
-                        <Text turncate={1} size={8}>
-                          {data.staff[0].name}
-                        </Text>
-                        <Text turncate={1} size={7} color='info'>
-                          {data.staff[0].position}
-                        </Text>
+        {data.staff && data.staff.length > 0 && (
+          <View style={{ paddingHorizontal: theme.units.md }}>
+            <Div filled title='staff' radii='md'>
+              <BottomSheet
+                handleSheetChange={handleSheetChanges}
+                triggerElement={
+                  <View style={{ position: 'relative' }}>
+                    <Div>
+                      <View style={{ flexDirection: 'row', alignItems: 'center', gap: theme.units.sb }}>
+                        <Image source={`${variables.STORAGE_LINK}/bars/staff/${data.staff[0].image}`} height='32px' width='32px' radii='rounded' />
+                        <View>
+                          <Text turncate={1} size={8}>
+                            {data.staff[0].name}
+                          </Text>
+                          <Text turncate={1} size={7} color='info'>
+                            {data.staff[0].position}
+                          </Text>
+                        </View>
                       </View>
+                    </Div>
+                    <View style={{ width: '100%', alignItems: 'center' }}>
+                      <Feather name='chevron-down' size={14} color='black' />
                     </View>
-                  </Div>
-                  <View style={{ width: '100%', alignItems: 'center' }}>
-                    <Feather name='chevron-down' size={14} color='black' />
                   </View>
-                </View>
-              }>
-              <Animated.View style={{ ...animatedStyle }}>
-                <Text t={'capitalize'} size={10} weight='md'>{`List of ${data.title} staff`}</Text>
-                {data.staff.map((staff, index) => (
-                  <Div filled key={index}>
-                    <View style={{ flexDirection: 'row', alignItems: 'center', gap: theme.units.sb }}>
-                      <Image source={staff.image} height='28px' width='28px' radii='rounded' />
-                      <View>
-                        <Text turncate={1} size={8}>
-                          {staff.name}
-                        </Text>
-                        <Text turncate={1} size={7} color='info'>
-                          {staff.position}
-                        </Text>
+                }>
+                <Animated.View style={{ ...animatedStyle }}>
+                  <Text t={'capitalize'} size={10} weight='md'>{`List of ${data.title} staff`}</Text>
+                  {data.staff.map((staff, index) => (
+                    <Div filled key={index}>
+                      <View style={{ flexDirection: 'row', alignItems: 'center', gap: theme.units.sb }}>
+                        <Image source={`${variables.STORAGE_LINK}/bars/staff/${staff.image}`} height='28px' width='28px' radii='rounded' />
+                        <View>
+                          <Text turncate={1} size={8}>
+                            {staff.name}
+                          </Text>
+                          <Text turncate={1} size={7} color='info'>
+                            {staff.position}
+                          </Text>
+                        </View>
                       </View>
-                    </View>
-                  </Div>
-                ))}
-              </Animated.View>
-            </BottomSheet>
-          </Div>
-        </View>
+                    </Div>
+                  ))}
+                </Animated.View>
+              </BottomSheet>
+            </Div>
+          </View>
+        )}
 
         {
           /* menu */
@@ -152,7 +174,7 @@ export default ({ navigation, route }) => {
                     navigation.navigate('[stack] stack-bar-detail-menu', {
                       id: data.id,
                       name: data.title,
-                      data: data.menu_a_la_carte ? data.menu_a_la_carte : data.drinksMeun,
+                      data: data.menu_a_la_carte ? `${variables.STORAGE_LINK}/pdf/menus/bar/${data.menu_a_la_carte}` : data.menu,
                     })
                   }>
                   <Icon icon={require('@/assets/icons/product/bar/drinks.png')} size={42} />
@@ -166,7 +188,16 @@ export default ({ navigation, route }) => {
         }
       </Container>
       {/* can make reservation */}
-      {data.reservation_required && <Button style={{ marginBottom: theme.units.lg, marginTop: theme.units.sm, marginHorizontal: theme.units.md }}>make reservation</Button>}
+      {data.reservation_required === 1 && <Button style={{ marginBottom: theme.units.lg, marginTop: theme.units.sm, marginHorizontal: theme.units.md }}>make reservation</Button>}
     </View>
   )
+}
+
+const getClientSideQueries = {
+  getBarById: (id) =>
+    useFetch(`/api/bars/${id}`)
+      .then((res) => res.data)
+      .catch((err) => {
+        throw new Error(err)
+      }),
 }
--- page/bar/screens/bar.jsx
+++ page/bar/screens/bar.jsx
@@ -1,6 +1,7 @@
 /* packages */
 import moment from 'moment'
-import { View } from 'react-native'
+import { useState, useCallback } from 'react'
+import { RefreshControl, View } from 'react-native'
 
 /* modules */
 import { Text } from '@/common'
@@ -9,25 +10,41 @@ import { FlatList } from '@/shared'
 /* components */
 import { OptionCard } from '@/components'
 
-/* styles */
+/* utilities */
+import { variables } from '@/constant/variables'
 
 /* mocks */
-import { data } from '@/mocks/bar.data'
+import { useFetch } from '@/hook/useFetch'
+import { useQuery } from 'react-query'
 
 export default ({ navigation }) => {
+  /* data fetching */
+  const [refresh, setRefresh] = useState(false)
+
+  const { data, isLoading, error, refetch } = useQuery('@Bars', getClientSideQueries.getBars)
+
+  let onRefresh = useCallback(() => {
+    setRefresh(true)
+    refetch().then(() => setRefresh(false))
+  }, [])
+
+  if (isLoading) return <Text>Loading...</Text>
+
+  if (error) return <Text>Error: {error.message}</Text>
+
   return (
     <View style={{ flex: 1 }}>
       <FlatList
-        data={data.bars}
+        data={data}
         gap='md'
         keyExtractor={(item) => item.id}
         renderedItem={({ item }) => (
           <OptionCard
             key={item.id}
-            image={item.images[0].image}
-            closed={!isBetweenOpenAndClose(item.timing.open, item.timing.close)}
-            adults={item.adults_only}
-            reservation={item.reservation_required}
+            image={`${variables.STORAGE_LINK}/bars/${item.images[0].image}`}
+            closed={!isBetweenOpenAndClose(item.timing_open, item.timing_close)}
+            adults={item.adults_only ? true : false}
+            reservation={item.reservation_required ? true : false}
             onPress={() =>
               navigation.navigate('[stack] stack-bar-detail', {
                 id: item.id,
@@ -39,7 +56,7 @@ export default ({ navigation }) => {
                 {item.title}
               </Text>
               <Text t='uppercase' color='sub' weight='md'>
-                {isBetweenOpenAndClose(item.timing.open, item.timing.close) ? `Closes at ${item.timing.close}` : `Opens at ${item.timing.open}`}
+                {isBetweenOpenAndClose(item.timing_open, item.timing_close) ? `Closes at ${item.timing_close}` : `Opens at ${item.timing_open}`}
               </Text>
             </View>
 
@@ -50,6 +67,7 @@ export default ({ navigation }) => {
             </View>
           </OptionCard>
         )}
+        refreshControl={<RefreshControl refreshing={refresh} onRefresh={onRefresh} />}
       />
     </View>
   )
@@ -59,4 +77,13 @@ const isBetweenOpenAndClose = (openTime, closeTime) => {
   if (moment(closeTime, 'HH:mm').isBefore(moment(openTime, 'HH:mm')))
     return moment().isBetween(moment(openTime, 'HH:mm'), moment('23:59', 'HH:mm'), 'minutes', '[]') || moment().isBetween(moment('00:00', 'HH:mm'), moment(closeTime, 'HH:mm'), 'minutes', '[]')
   return moment().isBetween(moment(openTime, 'HH:mm'), moment(closeTime, 'HH:mm'), 'minutes', '[]')
+}
+
+const getClientSideQueries = {
+  getBars: () =>
+    useFetch('/api/bars?query=1')
+      .then((res) => res.data)
+      .catch((err) => {
+        throw new Error(err)
+      }),
 }
