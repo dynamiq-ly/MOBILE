e7d4605744012471cab2539fa7cb5e8fd250dbf8
Manel Rejeb <91155171+Manel-Rejeb@users.noreply.github.com>
2023-08-19 13:04:27 +0200

    adding more information to the menu list details component

--- page/restaurant/screens/restaurant-details-menu-list-detail.jsx
+++ page/restaurant/screens/restaurant-details-menu-list-detail.jsx
@@ -6,12 +6,16 @@ import { Card } from '@/components'
 
 /* utilities */
 import { FlatList } from '@/shared'
-import { Text } from '@/common'
+import { Container, Text, Image } from '@/common'
 
 /* styles */
 import { useTheme } from 'styled-components'
 
+/* mocks */
+import { category as data } from '@/mocks/restaurant.data'
+
 export default ({ navigation, route }) => {
+  const theme = useTheme()
   const { data } = route.params
 
   return (
@@ -21,20 +25,44 @@ export default ({ navigation, route }) => {
         gap='md'
         keyExtractor={(_, index) => index}
         renderedItem={({ item }) => (
-          <Card padding={false} image={item.image}>
-            <Text size={10} weight='md' t='capitalize'>
-              {item.label}
-            </Text>
+          <Card
+            padding={false}
+            image={item.image}
+            onPress={() =>
+              navigation.navigate('[stack] stack-restaurant-detail-menu-list-detail-menu', {
+                id: item.id,
+                data: item,
+                previousScreen: route.params ? route.params.previousScreen : null,
+              })
+            }>
+            <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
+              <Text size={10} weight='md' t='capitalize'>
+                {item.label}
+              </Text>
+              <Text size={8} line={1.25} color='info' turncate={1} t='capitalize' weight='md'>
+                {item.price}
+              </Text>
+            </View>
             <Text size={6} line={1.25} color='small' turncate={1}>
               {item.slug}
             </Text>
-            <Text size={6} line={1.25} color='small' turncate={1}>
-              {item.price}
-            </Text>
+            <View style={{ flex: 1, flexDirection: 'row', alignItems: 'center', gap: 30 }}>
+              <View style={{ flexDirection: 'row', alignItems: 'center', gap: theme.units.sm }}>
+                <Image source={require('@/assets/icons/product/monocrome/leaf-dark.png')} height='16px' width='16px' />
+                <Text size={6} weight='md'>
+                  {item.category}
+                </Text>
+              </View>
+              <View style={{ flexDirection: 'row', alignItems: 'center', gap: theme.units.sm }}>
+                <Image source={require('@/assets/icons/product/monocrome/clock-dark.png')} height='16px' width='16px' />
+                <Text size={6} weight='md'>
+                  {item.time}
+                </Text>
+              </View>
+            </View>
           </Card>
         )}
       />
     </View>
   )
 }
-
